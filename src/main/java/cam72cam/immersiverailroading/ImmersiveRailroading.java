0c5319ffab67555d17f892ca9d08c6d8ee919b7c
Christian Mesh <christianmesh1@gmail.com>
2017-07-09 17:38:31 -0400

    Create Immersive Railroading based on forge example

---
+++ src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
@@ -0,0 +1,17 @@
+package cam72cam.immersiverailroading;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ImmersiveRailroading.MODID, version = ImmersiveRailroading.VERSION)
+public class ImmersiveRailroading
+{
+    public static final String MODID = "immersiverailroading";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
