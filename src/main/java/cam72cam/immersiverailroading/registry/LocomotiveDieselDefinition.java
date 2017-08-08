513cdb219eb017a488a182bbc1fa5199fd462353
Christian Mesh <christianmesh1@gmail.com>
2017-08-08 18:50:56 -0400

    Move registry outside of entity folder

--- src/main/java/cam72cam/immersiverailroading/registry/LocomotiveDieselDefinition.java
+++ src/main/java/cam72cam/immersiverailroading/registry/LocomotiveDieselDefinition.java
@@ -1,17 +1,17 @@
-package cam72cam.immersiverailroading.entity.registry;
+package cam72cam.immersiverailroading.registry;
 
 import com.google.gson.JsonObject;
 
-import cam72cam.immersiverailroading.entity.LocomotiveElectric;
+import cam72cam.immersiverailroading.entity.LocomotiveDiesel;
 import cam72cam.immersiverailroading.entity.EntityRollingStock;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class LocomotiveElectricDefinition extends LocomotiveDefinition {
+public class LocomotiveDieselDefinition extends LocomotiveDefinition {
 	private int fuelCapacity;
 
-	public LocomotiveElectricDefinition(String defID, JsonObject data) throws Exception {
+	public LocomotiveDieselDefinition(String defID, JsonObject data) throws Exception {
 		super(defID, data);
 		JsonObject properties = data.get("properties").getAsJsonObject();
 		fuelCapacity = properties.get("fuel_capacity").getAsInt();
@@ -19,7 +19,7 @@ public class LocomotiveElectricDefinition extends LocomotiveDefinition {
 
 	@Override
 	public EntityRollingStock spawn(World world, BlockPos pos, EnumFacing facing) {
-		LocomotiveElectric loco = new LocomotiveElectric(world, defID);
+		LocomotiveDiesel loco = new LocomotiveDiesel(world, defID);
 
 		loco.setPosition(pos.getX(), pos.getY(), pos.getZ());
 		loco.prevRotationYaw = facing.getHorizontalAngle();
--- src/main/java/cam72cam/immersiverailroading/registry/LocomotiveElectricDefinition.java
+++ src/main/java/cam72cam/immersiverailroading/registry/LocomotiveElectricDefinition.java
@@ -1,29 +1,25 @@
-package cam72cam.immersiverailroading.entity.registry;
+package cam72cam.immersiverailroading.registry;
 
 import com.google.gson.JsonObject;
 
+import cam72cam.immersiverailroading.entity.LocomotiveElectric;
 import cam72cam.immersiverailroading.entity.EntityRollingStock;
-import cam72cam.immersiverailroading.entity.LocomotiveSteam;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class LocomotiveSteamDefinition extends LocomotiveDefinition {
-	private int waterConsumption;
-	private int tankCapacity;
+public class LocomotiveElectricDefinition extends LocomotiveDefinition {
 	private int fuelCapacity;
 
-	public LocomotiveSteamDefinition(String defID, JsonObject data) throws Exception {
+	public LocomotiveElectricDefinition(String defID, JsonObject data) throws Exception {
 		super(defID, data);
 		JsonObject properties = data.get("properties").getAsJsonObject();
-		waterConsumption = properties.get("fuel_consumption").getAsInt();
-		tankCapacity = properties.get("water_capacity").getAsInt();
 		fuelCapacity = properties.get("fuel_capacity").getAsInt();
 	}
 
 	@Override
 	public EntityRollingStock spawn(World world, BlockPos pos, EnumFacing facing) {
-		LocomotiveSteam loco = new LocomotiveSteam(world, defID);
+		LocomotiveElectric loco = new LocomotiveElectric(world, defID);
 
 		loco.setPosition(pos.getX(), pos.getY(), pos.getZ());
 		loco.prevRotationYaw = facing.getHorizontalAngle();
@@ -31,14 +27,6 @@ public class LocomotiveSteamDefinition extends LocomotiveDefinition {
 		world.spawnEntity(loco);
 
 		return loco;
-	}
-
-	public int getWaterConsumption() {
-		return this.waterConsumption;
-	}
-
-	public int getTankCapacity() {
-		return this.tankCapacity;
 	}
 	
 	public int getFuelCapacity() {
