ce5bd7e55d2440c1f2b4444ef717b6604b1f9b0b
Christian Mesh <christianmesh1@gmail.com>
2017-08-12 08:36:40 -0400

    Better steam loco bogey turning

--- src/main/java/cam72cam/immersiverailroading/render/StockModel.java
+++ src/main/java/cam72cam/immersiverailroading/render/StockModel.java
@@ -3,13 +3,17 @@ package cam72cam.immersiverailroading.render;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.lwjgl.util.vector.Vector3f;
+
 import cam72cam.immersiverailroading.entity.EntityMoveableRollingStock;
 import cam72cam.immersiverailroading.entity.EntityRollingStock;
 import cam72cam.immersiverailroading.entity.LocomotiveSteam;
 import cam72cam.immersiverailroading.registry.EntityRollingStockDefinition;
+import cam72cam.immersiverailroading.render.obj.Face;
 import cam72cam.immersiverailroading.render.obj.OBJModel;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.Vec3d;
 
 public class StockModel extends OBJModel {
 	public StockModel(ResourceLocation modelLoc) throws Exception {
@@ -77,6 +81,59 @@ public class StockModel extends OBJModel {
 
 
 	private void drawSteamLocomotive(LocomotiveSteam stock) {
-		drawStandardStock(stock);
+		if (stock.frontYaw == null || stock.rearYaw == null) {
+			draw();
+			return;
+		}
+
+		EntityRollingStockDefinition def = stock.getDefinition();
+
+		List<String> main = new ArrayList<String>();
+		List<String> front = new ArrayList<String>();
+		List<String> rear = new ArrayList<String>();
+
+		for (String group : groups()) {
+			if (group.contains("BOGEY_FRONT")) {
+				front.add(group);
+			} else if (group.contains("BOGEY_REAR")) {
+				rear.add(group);
+			} else {
+				main.add(group);
+			}
+		}
+
+		drawGroups(main);
+
+		
+		if (front.size() != 0) {
+			Vector3f frontVec = centerOfGroups(front);
+	
+			GlStateManager.pushMatrix();
+			GlStateManager.translate(-frontVec.z, 0, 0);
+			if (!stock.isReverse) {
+				GlStateManager.rotate(180 - stock.frontYaw, 0, 1, 0);
+			} else {
+				GlStateManager.rotate(180 - stock.rearYaw, 0, 1, 0);
+			}
+			GlStateManager.rotate(-(180 - stock.rotationYaw), 0, 1, 0);
+			GlStateManager.translate(frontVec.z, 0, 0);
+			drawGroups(front);
+			GlStateManager.popMatrix();
+		}
+
+		if (rear.size() != 0) {
+			Vector3f rearVec = centerOfGroups(rear);
+			GlStateManager.pushMatrix();
+			GlStateManager.translate(-rearVec.z, 0, 0);
+			if (!stock.isReverse) {
+				GlStateManager.rotate(180 - stock.rearYaw, 0, 1, 0);
+			} else {
+				GlStateManager.rotate(180 - stock.frontYaw, 0, 1, 0);
+			}
+			GlStateManager.rotate(-(180 - stock.rotationYaw), 0, 1, 0);
+			GlStateManager.translate(rearVec.z, 0, 0);
+			drawGroups(rear);
+			GlStateManager.popMatrix();
+		}
 	}
 }
