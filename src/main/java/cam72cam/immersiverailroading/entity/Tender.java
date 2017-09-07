85d278e3783f16c53a3b37a0b857fc1537ed0589
Christian Mesh <christianmesh1@gmail.com>
2017-09-07 08:30:18 -0400

    Add tender GUI

--- src/main/java/cam72cam/immersiverailroading/entity/Tender.java
+++ src/main/java/cam72cam/immersiverailroading/entity/Tender.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cam72cam.immersiverailroading.library.GuiTypes;
+import cam72cam.immersiverailroading.registry.DefinitionManager;
+import cam72cam.immersiverailroading.registry.TenderDefinition;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.*;
 import net.minecraftforge.fluids.capability.IFluidHandler;
@@ -18,9 +20,8 @@ public class Tender extends CarTank implements IFluidHandler {
 		super(world, defID);
 	}
 	
-	@Override
-	public int getInventorySize() {
-		return 16;
+	public TenderDefinition getDefinition() {
+		return (TenderDefinition) DefinitionManager.getDefinition(defID);
 	}
 	
 	@Override
@@ -33,5 +34,23 @@ public class Tender extends CarTank implements IFluidHandler {
 		List<Fluid> filter = new ArrayList<Fluid>();
 		filter.add(FluidRegistry.WATER);
 		return filter;
+	}
+
+	@Override
+	public int getInventorySize() {
+		return this.getDefinition().getInventorySize() + 2;
+	}
+	
+	public int getInventoryWidth() {
+		return this.getDefinition().getInventoryWidth();
+	}
+	
+	@Override
+	protected int[] getContainerInputSlots() {
+		return new int[] { getInventorySize()-2 };
+	}
+	@Override
+	protected int[] getContainertOutputSlots() {
+		return new int[] { getInventorySize()-1 };
 	}
 }
--- src/main/java/cam72cam/immersiverailroading/registry/TenderDefinition.java
+++ src/main/java/cam72cam/immersiverailroading/registry/TenderDefinition.java
@@ -2,10 +2,40 @@ package cam72cam.immersiverailroading.registry;
 
 import com.google.gson.JsonObject;
 
+import cam72cam.immersiverailroading.entity.EntityRollingStock;
+import cam72cam.immersiverailroading.entity.Tender;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
 public class TenderDefinition extends CarTankDefinition {
+	private int numSlots;
+	private int width;
 
 	public TenderDefinition(String defID, JsonObject data) throws Exception {
 		super(defID, data);
+		JsonObject tender = data.get("tender").getAsJsonObject();
+		this.numSlots = tender.get("slots").getAsInt();
+		this.width = tender.get("width").getAsInt();
+	}
+	
+	@Override
+	public EntityRollingStock spawn(World world, BlockPos pos, EnumFacing facing) {
+		Tender loco = new Tender(world, defID);
+
+		loco.setPosition(pos.getX(), pos.getY(), pos.getZ());
+		loco.prevRotationYaw = facing.getHorizontalAngle();
+		loco.rotationYaw = facing.getHorizontalAngle();
+		world.spawnEntity(loco);
+
+		return loco;
+	}
+	
+	public int getInventorySize() {
+		return numSlots;
 	}
 
+	public int getInventoryWidth() {
+		return width;
+	}
 }
