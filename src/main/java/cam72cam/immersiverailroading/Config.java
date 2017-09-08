2dc0314c3ac7a128953370520c8ee8d232e89079
Christian Mesh <christianmesh1@gmail.com>
2017-09-08 07:41:17 -0400

    Move stock list into stock.json in resource pack

--- src/main/java/cam72cam/immersiverailroading/Config.java
+++ src/main/java/cam72cam/immersiverailroading/Config.java
@@ -7,21 +7,6 @@ public class Config {
 	@Comment({"Range between couplers to try coupling"})
 	public static double couplerRange = 0.3;
 
-	@Comment({"A list of locomotives to load"})
-	public static String[] locomotives = new String[] {"gp-7", "a1_peppercorn", "big_boy"};
-	
-	@Comment({"A list of tenders to load"})
-	public static String[] tenders = new String[] { "a1_peppercorn_tender" };
-	
-	@Comment({"A list of passenger cars to load"})
-	public static String[] passenger_cars = new String[] { "br_coach_mk1" };
-	
-	@Comment({"A list of freight cars to load"})
-	public static String[] freight_cars = new String[] { "boxcar_us", "flatcar_us" };
-	
-	@Comment({"A list of tank cars to load"})
-	public static String[] tank_cars = new String[] { "tank_us" };
-
 	@Comment({"km/h to damage 1 heart on collision"})
 	public static double entitySpeedDamage = 10;
 	
