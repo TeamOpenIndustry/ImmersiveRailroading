8718fc41514f41bd4faaf7f9c0aebfe23e729b72
Christian Mesh <christianmesh1@gmail.com>
2017-09-08 18:45:55 -0400

    Steam slots and burning update

--- src/main/java/cam72cam/immersiverailroading/entity/LocomotiveSteam.java
+++ src/main/java/cam72cam/immersiverailroading/entity/LocomotiveSteam.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import cam72cam.immersiverailroading.library.GuiTypes;
 import cam72cam.immersiverailroading.registry.DefinitionManager;
