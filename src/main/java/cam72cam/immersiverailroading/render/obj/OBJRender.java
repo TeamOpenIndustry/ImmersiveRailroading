c7a1dde148cf7fe8661d5d5219e4fa2a93f77089
Christian Mesh <christianmesh1@gmail.com>
2017-09-12 07:37:01 -0400

    Decent performance boost by grouping the draw calls

--- src/main/java/cam72cam/immersiverailroading/render/obj/OBJRender.java
+++ src/main/java/cam72cam/immersiverailroading/render/obj/OBJRender.java
@@ -1,5 +1,6 @@
 package cam72cam.immersiverailroading.render.obj;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,6 +8,8 @@ import java.util.Map;
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.model.ModelLoader.White;
 
 public class OBJRender {
 	
@@ -51,17 +54,50 @@ public class OBJRender {
 		}
 		GL11.glCallList(displayLists.get(groupNames));
 	}
-	
+
 	public void drawDirectGroups(Iterable<String> groupNames) {
-		GL11.glBegin(GL11.GL_QUADS);
-		int last = 4;
+		drawDirectGroups(groupNames, 1.0);
+	}
+
+	public void drawDirectGroups(Iterable<String> groupNames, double scale) {
+		Map<Integer, Map<Material, List<Face>>> stuff = new HashMap<Integer, Map<Material, List<Face>>>();
+		
 		for (String group : groupNames) {
 			Material currentMTL = model.materials.get(model.groupMtlMap.get(group));
 			List<Face> faces = model.groups.get(group);
-
-			if (currentMTL == null) {
-				//ImmersiveRailroading.logger.warn(String.format("Missing mtl %s %s", group, groupMtlMap.get(group)));
-			} else {
+			
+			for (Face face : faces) {
+				Integer type = -1;
+				switch (face.points.length) {
+				case 3:
+					type = GL11.GL_TRIANGLES;
+					break;
+				case 4:
+					type = GL11.GL_QUADS;
+					break;
+				default:
+					type = GL11.GL_POLYGON;
+					break;
+				}
+				
+				if (!stuff.containsKey(type)) {
+					stuff.put(type, new HashMap<Material, List<Face>>());
+				}
+				
+				if (!stuff.get(type).containsKey(currentMTL)) {
+					stuff.get(type).put(currentMTL, new ArrayList<Face>());
+				}
+				
+				stuff.get(type).get(currentMTL).add(face);
+			}
+		}
+		
+		for (Integer type : stuff.keySet()) {
+			GL11.glBegin(type);
+			boolean first = false;
+			Map<Material, List<Face>> stuffType = stuff.get(type);
+			for (Material currentMTL : stuffType.keySet()) {
+				List<Face> faces = stuffType.get(currentMTL);
 				if (currentMTL.Ka != null) {
 					GL11.glMaterial(GL11.GL_FRONT_AND_BACK, GL11.GL_AMBIENT, currentMTL.Ka);
 				}
@@ -76,53 +112,37 @@ public class OBJRender {
 				if (currentMTL.Ks != null) {
 					GL11.glMaterial(GL11.GL_FRONT_AND_BACK, GL11.GL_SPECULAR, currentMTL.Ks);
 				}
-			}
-
-			for (Face face : faces) {
-				switch (face.points.length) {
-				case 3:
-					if (face.points.length != last) {
+				for (Face face : faces) {
+					if (type == GL11.GL_POLYGON && !first) {
 						GL11.glEnd();
-						GL11.glBegin(GL11.GL_TRIANGLES);
+						GL11.glBegin(type);
 					}
-					break;
-				case 4:
-					if (face.points.length != last) {
-						GL11.glEnd();
-						GL11.glBegin(GL11.GL_QUADS);
-					}
-					break;
-				default:
-					GL11.glEnd();
-					GL11.glBegin(GL11.GL_POLYGON);
-					break;
-				}
-				last = face.points.length; 
+					first = false;
+					for (int[] point : face.points) {
+						Vec3d v;
+						Vec2f vt;
+						Vec3d vn;
 
-				for (int[] point : face.points) {
-					Vec3d v;
-					Vec2f vt;
-					Vec3d vn;
-
-					switch (point.length) {
-					case 3:
-						vn = model.vertexNormals.get(point[2]);
-						GL11.glNormal3d(vn.x, vn.y, vn.z);
-					case 2:
-						if (point[1] != -1) {
-							vt = model.vertexTextures.get(point[1]);
-							GL11.glTexCoord2f(vt.x, 1 - vt.y);
+						switch (point.length) {
+						case 3:
+							vn = model.vertexNormals.get(point[2]);
+							GL11.glNormal3d(vn.x, vn.y, vn.z);
+						case 2:
+							if (point[1] != -1) {
+								vt = model.vertexTextures.get(point[1]);
+								GL11.glTexCoord2f(vt.x, 1 - vt.y);
+							}
+						case 1:
+							v = model.vertices.get(point[0]);
+							GL11.glVertex3d(v.x*scale, v.y*scale, v.z*scale);
+							break;
+						default:
+							System.out.println("WATWATWAT");
 						}
-					case 1:
-						v = model.vertices.get(point[0]);
-						GL11.glVertex3d(v.x, v.y, v.z);
-						break;
-					default:
-						System.out.println("WATWATWAT");
 					}
 				}
 			}
+			GL11.glEnd();
 		}
-		GL11.glEnd();
 	}
 }
