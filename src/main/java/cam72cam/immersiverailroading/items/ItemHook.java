3324ff385ba9770ad33c1a2556746e76ef8afb2b
Christian Mesh <christianmesh1@gmail.com>
2017-09-22 15:07:40 -0400

    Improve stock coupling detection code

---
+++ src/main/java/cam72cam/immersiverailroading/items/ItemHook.java
@@ -0,0 +1,18 @@
+package cam72cam.immersiverailroading.items;
+
+import cam72cam.immersiverailroading.ImmersiveRailroading;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+//TODO buildcraft.api.tools.IToolWrench
+import net.minecraft.util.ResourceLocation;
+
+public class ItemHook extends Item {
+	public static final String NAME = "item_hook";
+	
+	public ItemHook() {
+		super();
+		setUnlocalizedName(ImmersiveRailroading.MODID + ":" + NAME);
+		setRegistryName(new ResourceLocation(ImmersiveRailroading.MODID, NAME));
+        this.setCreativeTab(CreativeTabs.TOOLS);
+	}
+}
