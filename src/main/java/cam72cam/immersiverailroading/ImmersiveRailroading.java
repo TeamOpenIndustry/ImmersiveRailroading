c2ad6c010e78f02c3452ef4dd0575e97be60e7a0
Christian Mesh <christianmesh1@gmail.com>
2017-12-04 20:16:49 -0500

    Beta 0.3.9

--- src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
+++ src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
@@ -34,7 +34,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 public class ImmersiveRailroading
 {
     public static final String MODID = "immersiverailroading";
-    public static final String VERSION = "0.3.8";
+    public static final String VERSION = "0.3.9";
 	public static final int ENTITY_SYNC_DISTANCE = 512;
     public static final String ORE_RAIL_BED = "railBed";
     
