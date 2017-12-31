5e572755f29fe17ec2dc2d6ce5b7b11faef05b93
Christian Mesh <christianmesh1@gmail.com>
2017-12-31 16:19:33 -0500

    Implement boiler rolling

--- src/main/java/cam72cam/immersiverailroading/render/multiblock/BoilerRollerRender.java
+++ src/main/java/cam72cam/immersiverailroading/render/multiblock/BoilerRollerRender.java
@@ -1,8 +1,12 @@
 package cam72cam.immersiverailroading.render.multiblock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.lwjgl.opengl.GL11;
 
 import cam72cam.immersiverailroading.model.obj.OBJModel;
+import cam72cam.immersiverailroading.multiblock.BoilerRollerMultiblock.BoilerRollerInstance;
 import cam72cam.immersiverailroading.render.OBJRender;
 import cam72cam.immersiverailroading.tile.TileMultiblock;
 import cam72cam.immersiverailroading.util.GLBoolTracker;
@@ -10,10 +14,25 @@ import net.minecraft.util.ResourceLocation;
 
 public class BoilerRollerRender implements IMultiblockRender {
 	private OBJRender renderer;
+	private List<String> segments;
+	private List<String> product;
+	private List<String> rest;
 
 	public BoilerRollerRender() {
 		try {
 			this.renderer = new OBJRender(new OBJModel(new ResourceLocation("immersiverailroading:models/multiblocks/boiler_rolling_machine.obj"), 0.1f));
+			segments = new ArrayList<String>();
+			product = new ArrayList<String>();
+			rest = new ArrayList<String>();
+			for (String name : renderer.model.groups.keySet()) {
+				if (name.contains("SEGMENT_")) {
+					segments.add(name);
+				} else if (name.contains("FINISHED_PREVIEW")) {
+					product.add(name);
+				} else {
+					rest.add(name);
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -22,12 +41,22 @@ public class BoilerRollerRender implements IMultiblockRender {
 	public void render(TileMultiblock te, double x, double y, double z, float partialTicks) {
 		GLBoolTracker tex = new GLBoolTracker(GL11.GL_TEXTURE_2D, false);
 		
+		BoilerRollerInstance tmb = (BoilerRollerInstance) te.getMultiblock();
+		
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
 		GL11.glTranslated(0.5, 0, 0.5);
 		GL11.glRotated(te.getRotation()-90, 0, 1, 0);
-		GL11.glTranslated(-3.35, 0, 0.5);
-		renderer.draw();
+		GL11.glTranslated(-3.35, 0, -2.5);
+		
+		//TODO better animation
+		if (tmb.hasOutput()) {
+			renderer.drawGroups(product);
+		} else if (tmb.hasInput()) {
+			renderer.drawGroups(segments);
+		}
+		
+		renderer.drawDirectGroups(rest);;
 		GL11.glPopMatrix();
 		
 		tex.restore();
