efb94064bac0b5550f96a0f6b507c5eb4df37444
Christian Mesh <christianmesh1@gmail.com>
2018-01-11 20:20:27 -0500

    Pure shader smoke

--- src/main/resources/assets/immersiverailroading/particles/smoke_frag.c
+++ src/main/resources/assets/immersiverailroading/particles/smoke_frag.c
@@ -1,18 +1,15 @@
-varying float DEPTH;
+uniform float ALPHA;
 uniform vec3 DARKEN;
 uniform sampler2D tex;
 
+varying vec4 v_texCoord;
+
 void main() {
-	vec3 ct;
+	vec3 ct,tmp;
 	vec4 texel;
 	float at,af,ad;
 
-	af = gl_FrontMaterial.diffuse.a;
-	
-	texel = texture2D(tex,gl_TexCoord[0].st);
-	ct = texel.rgb;
-	at = texel.a;
-	ad = DEPTH;
-	at = at * ad;
-	gl_FragColor = vec4(ct * DARKEN, at * af);
+	at = (( sin(v_texCoord.x * 3.1415) * sin(v_texCoord.y * 3.1415) ) + ( sin(v_texCoord.x * 20.0) * sin(v_texCoord.y * 20.0) / 20.0 )) * ALPHA;
+	tmp = DARKEN;
+	gl_FragColor = vec4(tmp, at);
 }
--- src/main/resources/assets/immersiverailroading/particles/smoke_vert.c
+++ src/main/resources/assets/immersiverailroading/particles/smoke_vert.c
@@ -1,13 +1,8 @@
-varying float DEPTH;
-uniform float ALPHA;
+
+varying vec4 v_texCoord;
 
 void main() {
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
     gl_TexCoord[0] = gl_MultiTexCoord0;
-    DEPTH = gl_Position.z / 100.0;
-    if (DEPTH > 1.0) {
-    	DEPTH = 1.0;
-    }
-    DEPTH = (1.0-DEPTH);
-    DEPTH = ALPHA;
+    v_texCoord = gl_MultiTexCoord0;
 }
