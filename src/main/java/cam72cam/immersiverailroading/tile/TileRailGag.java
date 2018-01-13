255b0e5ef74f9c043751973fec9f70b4ab9b8777
Christian Mesh <christianmesh1@gmail.com>
2018-01-13 09:35:15 -0500

    Probably fix #144

--- src/main/java/cam72cam/immersiverailroading/tile/TileRailGag.java
+++ src/main/java/cam72cam/immersiverailroading/tile/TileRailGag.java
@@ -3,7 +3,6 @@ package cam72cam.immersiverailroading.tile;
 public class TileRailGag extends TileRailBase {
 	public void setFlexible(boolean flexible) {
 		this.flexible = flexible;
-		this.markDirty();
 	}
 	
 	public boolean hasFastRenderer()
