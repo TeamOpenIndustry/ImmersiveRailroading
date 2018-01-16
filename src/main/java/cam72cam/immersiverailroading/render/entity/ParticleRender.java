7482715f1569a7faf65d14d6d3e6cd7fc78249bc
Christian Mesh <christianmesh1@gmail.com>
2018-01-15 19:03:44 -0500

    Much more efficient particle render

--- src/main/java/cam72cam/immersiverailroading/render/entity/ParticleRender.java
+++ src/main/java/cam72cam/immersiverailroading/render/entity/ParticleRender.java
@@ -4,7 +4,6 @@ import org.lwjgl.opengl.GL11;
 
 import cam72cam.immersiverailroading.entity.EntitySmokeParticle;
 import cam72cam.immersiverailroading.render.GLSLShader;
-import cam72cam.immersiverailroading.util.GLBoolTracker;
 import cam72cam.immersiverailroading.util.VecUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.culling.ICamera;
@@ -15,12 +14,31 @@ import net.minecraft.util.math.Vec3d;
 
 public class ParticleRender extends Render<EntitySmokeParticle> {
 
-	private GLSLShader shader;
+	public static GLSLShader shader;
+	private int dl;
 
 	public ParticleRender(RenderManager renderManager) {
 		super(renderManager);
 		
 		shader = new GLSLShader("smoke_vert.c", "smoke_frag.c");
+		
+		dl = GL11.glGenLists(1);
+		GL11.glNewList(dl, GL11.GL_COMPILE);
+		{
+			GL11.glBegin(GL11.GL_QUADS);
+			{
+				GL11.glTexCoord2d(0, 0);
+				GL11.glVertex3d(-1, -1, 0);
+				GL11.glTexCoord2d(0, 1);
+				GL11.glVertex3d(-1, 1, 0);
+				GL11.glTexCoord2d(1, 1);
+				GL11.glVertex3d(1, 1, 0);
+				GL11.glTexCoord2d(1, 0);
+				GL11.glVertex3d(1, -1, 0);
+			}
+			GL11.glEnd();
+		}
+		GL11.glEndList();
 	}
 
 	@Override
@@ -30,22 +48,14 @@ public class ParticleRender extends Render<EntitySmokeParticle> {
 
 	@Override
 	public void doRender(EntitySmokeParticle particle, double x, double y, double z, float entityYaw, float partialTicks) {
-		Minecraft.getMinecraft().mcProfiler.startSection("ir_particles");
 		GL11.glPushMatrix();
 		{
 			float darken = 0.9f-particle.darken*0.9f;
 			float alpha = particle.alpha;//(particle.lifespan - particle.ticksExisted - partialTicks)/(float)particle.lifespan * (0.2f + particle.thickness * 2);
 			double size = particle.radius; //(particle.ticksExisted + partialTicks) / 20.0 * particle.size * (particle.motionY*8);
 			
-			shader.bind();
 			shader.paramFloat("ALPHA", alpha);
 			shader.paramFloat("DARKEN", darken, darken, darken);
-
-			GLBoolTracker light = new GLBoolTracker(GL11.GL_LIGHTING, false);
-			GLBoolTracker cull = new GLBoolTracker(GL11.GL_CULL_FACE, false);
-			GLBoolTracker tex = new GLBoolTracker(GL11.GL_TEXTURE_2D, false);
-			GLBoolTracker blend = new GLBoolTracker(GL11.GL_BLEND, true);
-			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 			// Move to specified position
 			GL11.glTranslated(x, y, z);
@@ -69,28 +79,9 @@ public class ParticleRender extends Render<EntitySmokeParticle> {
 			GL11.glRotated(angle, 0, 0, 1);
 
 			//Draw
-			GL11.glBegin(GL11.GL_QUADS);
-			{
-				GL11.glTexCoord2d(0, 0);
-				GL11.glVertex3d(-1, -1, 0);
-				GL11.glTexCoord2d(0, 1);
-				GL11.glVertex3d(-1, 1, 0);
-				GL11.glTexCoord2d(1, 1);
-				GL11.glVertex3d(1, 1, 0);
-				GL11.glTexCoord2d(1, 0);
-				GL11.glVertex3d(1, -1, 0);
-			}
-			GL11.glEnd();
-
-			blend.restore();
-			tex.restore();
-			cull.restore();
-			light.restore();
-			
-			shader.unbind();
+			GL11.glCallList(dl);
 		}
 		GL11.glPopMatrix();
-		Minecraft.getMinecraft().mcProfiler.endSection();
 	}
 
 	@Override
