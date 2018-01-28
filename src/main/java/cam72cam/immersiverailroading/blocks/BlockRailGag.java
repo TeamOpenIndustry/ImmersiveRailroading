b6d7fccad2b693296e5e80986021f9c4be551cc1
Christian Mesh <christianmesh1@gmail.com>
2018-01-28 17:51:14 -0500

    Remove unused imports

--- src/main/java/cam72cam/immersiverailroading/blocks/BlockRailGag.java
+++ src/main/java/cam72cam/immersiverailroading/blocks/BlockRailGag.java
@@ -3,13 +3,10 @@ package cam72cam.immersiverailroading.blocks;
 import java.util.Random;
 
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import cam72cam.immersiverailroading.ImmersiveRailroading;
 import cam72cam.immersiverailroading.tile.TileRailGag;
