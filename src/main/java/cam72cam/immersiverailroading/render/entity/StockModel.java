f4b7b8975effec7406bea17b038fd6ae81b86a6e
Christian Mesh <christianmesh1@gmail.com>
2018-03-25 15:23:39 -0400

    Fix #146: Add fan component for diesels
    I'll do the animation some other time

--- src/main/java/cam72cam/immersiverailroading/render/entity/StockModel.java
+++ src/main/java/cam72cam/immersiverailroading/render/entity/StockModel.java
@@ -191,6 +191,7 @@ public class StockModel extends OBJRender {
 		drawComponent(def.getComponent(RenderComponentType.FUEL_TANK, stock.gauge));
 		drawComponent(def.getComponent(RenderComponentType.ALTERNATOR, stock.gauge));
 		drawComponent(def.getComponent(RenderComponentType.ENGINE_BLOCK, stock.gauge));
+		drawComponents(def.getComponents(RenderComponentType.FAN_X, stock.gauge));
 		
 		drawComponents(def.getComponents(RenderComponentType.CAB, stock.gauge));
 		drawComponents(def.getComponents(RenderComponentType.WHISTLE, stock.gauge));
