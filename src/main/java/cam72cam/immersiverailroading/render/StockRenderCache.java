8422b63b02462cb295ef0d4f3df6e7b3588c5cf7
Christian Mesh <christianmesh1@gmail.com>
2018-03-29 20:56:52 -0400

    Better render priming

--- src/main/java/cam72cam/immersiverailroading/render/StockRenderCache.java
+++ src/main/java/cam72cam/immersiverailroading/render/StockRenderCache.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.lwjgl.opengl.GL11;
 import cam72cam.immersiverailroading.ConfigGraphics;
 import cam72cam.immersiverailroading.ImmersiveRailroading;
+import cam72cam.immersiverailroading.proxy.ClientProxy;
 import cam72cam.immersiverailroading.registry.DefinitionManager;
 import cam72cam.immersiverailroading.registry.EntityRollingStockDefinition;
 import cam72cam.immersiverailroading.render.entity.StockModel;
@@ -15,6 +16,7 @@ import cam72cam.immersiverailroading.util.GLBoolTracker;
 public class StockRenderCache {
 	private static Map<String, StockModel> render_cache = new HashMap<String, StockModel>();
 	private static IconTextureSheet icon_cache;
+	private static boolean isCachePrimed = false;
 
 	public static void clearRenderCache() {
 		for (StockModel model : render_cache.values()) {
@@ -22,18 +24,33 @@ public class StockRenderCache {
 		}
 		render_cache = new HashMap<String, StockModel>();
 		icon_cache = null;
-		primeCache();
+		isCachePrimed = false;
 	}
 
-	public static void primeCache() {
+	private static void tryPrimeRenderCache() {
+		if(isCachePrimed) {
+			return;
+		}
+		isCachePrimed = true;
+		
 		for (String def : DefinitionManager.getDefinitionNames()) {
 			ImmersiveRailroading.info("Priming Render Cache: %s", def);;
-			getRender(def);
+			StockModel renderer = getRender(def);
+			if (ConfigGraphics.enableItemRenderPriming) {
+				renderer.bindTexture();
+				renderer.draw();
+				renderer.restoreTexture();
+				ClientProxy.renderCacheLimiter.reset();
+			}
 		}
 	}
 
-	public static void doImageCache() {
-		if (ConfigGraphics.enableIconCache && icon_cache == null) {
+	private static void tryPrimeImageCache() {
+		if (icon_cache != null) {
+			return;
+		}
+		
+		if (ConfigGraphics.enableIconCache) {
 			System.out.println("Initializing Icon Cache...");
 			icon_cache = new IconTextureSheet();
 			for (String defID : DefinitionManager.getDefinitionNames()) {
@@ -73,4 +90,8 @@ public class StockRenderCache {
 		return render_cache.get(defID);
 	}
 
+	public static void tryPrime() {
+		tryPrimeRenderCache();
+		tryPrimeImageCache();
+	}
 }
