d0a7f57e4fcd30e9877524538c06aae22988b087
Christian Mesh <christianmesh1@gmail.com>
2018-04-09 08:08:24 -0400

    Fix dupe bug and shift + clicking

--- src/main/java/cam72cam/immersiverailroading/gui/SteamHammerContainer.java
+++ src/main/java/cam72cam/immersiverailroading/gui/SteamHammerContainer.java
@@ -2,9 +2,7 @@ package cam72cam.immersiverailroading.gui;
 
 import cam72cam.immersiverailroading.items.nbt.ItemRawCast;
 import cam72cam.immersiverailroading.tile.TileMultiblock;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
@@ -33,39 +31,9 @@ public class SteamHammerContainer extends ContainerBase {
     	currY = offsetPlayerInventoryConnector(0, currY, width, horizSlots);
     	currY = addPlayerInventory(playerInventory, currY, horizSlots);
 	}
-
-	@Override
-	public boolean canInteractWith(EntityPlayer playerIn) {
-		return true;
-	}
-
 	
-    @Override
-    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
-        ItemStack itemstack = null;
-        Slot slot = this.inventorySlots.get(index);
-
-        if (slot != null && slot.getHasStack()) {
-            ItemStack itemstack1 = slot.getStack();
-            itemstack = itemstack1.copy();
-            if (index <= 2) {
-            	if (!this.mergeItemStack(itemstack1, 2, this.inventorySlots.size(), false)) {
-                    return ItemStack.EMPTY;
-                }
-            } else {
-            	if (!this.mergeItemStack(itemstack1, 0, 1, false)) {
-                    return ItemStack.EMPTY;
-                }
-            }
-            
-
-            if (itemstack1.isEmpty()) {
-                slot.putStack(ItemStack.EMPTY);
-            } else {
-                slot.onSlotChanged();
-            }
-        }
-
-        return itemstack;
-    }
+	@Override
+	public int numSlots() {
+		return 2;
+	}
 }
