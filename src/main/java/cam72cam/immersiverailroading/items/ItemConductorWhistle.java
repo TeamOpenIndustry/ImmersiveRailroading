5de2020b4eacd9de0edef56bb49bb1eec96d080a
Christian Mesh <christianmesh1@gmail.com>
2018-06-02 21:30:40 -0400

    Conductor's whistle works server side now (broadcast audio)

--- src/main/java/cam72cam/immersiverailroading/items/ItemConductorWhistle.java
+++ src/main/java/cam72cam/immersiverailroading/items/ItemConductorWhistle.java
@@ -1,12 +1,14 @@
 package cam72cam.immersiverailroading.items;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 import cam72cam.immersiverailroading.Config;
 import cam72cam.immersiverailroading.ImmersiveRailroading;
 import cam72cam.immersiverailroading.entity.EntityCoupleableRollingStock;
 import cam72cam.immersiverailroading.library.Gauge;
-import cam72cam.immersiverailroading.sound.ISound;
+import cam72cam.immersiverailroading.net.SoundPacket;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -15,11 +17,13 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.NetworkRegistry.TargetPoint;
 
 public class ItemConductorWhistle extends Item {
 	public static final String NAME = "item_conductor_whistle";
-	private static ISound whistle;
+	private static HashMap<UUID, Integer> cooldown = new HashMap<UUID, Integer>();
 	
 	public ItemConductorWhistle() {
 		super();
@@ -30,16 +34,27 @@ public class ItemConductorWhistle extends Item {
 	
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
-		if (world.isRemote) {
-			if (whistle == null) {
-				whistle = ImmersiveRailroading.proxy.newSound(new ResourceLocation(ImmersiveRailroading.MODID, "sounds/conductor_whistle.ogg"), false, (float) Config.ConfigBalance.villagerConductorDistance * 1.2f, Gauge.STANDARD);
+		if (!world.isRemote) {
+			if (cooldown.containsKey(player.getPersistentID())) {
+				int newtime = cooldown.get(player.getPersistentID());
+				if (newtime < ImmersiveRailroading.proxy.getTicks()) {
+					cooldown.remove(player.getPersistentID());
+				} else {
+					return super.onItemRightClick(world, player, hand);
+				}
 			}
-			if (!whistle.isPlaying()) {
-				whistle.setPitch((float) (Math.random() / 4 + 0.75));
-				whistle.setVolume(0.7f);
-				whistle.play(player.getPositionVector());
-			}
-		} else {
+			
+			cooldown.put(player.getPersistentID(), ImmersiveRailroading.proxy.getTicks() + 40);
+			
+			SoundPacket packet = new SoundPacket(
+					new ResourceLocation(ImmersiveRailroading.MODID, "sounds/conductor_whistle.ogg").toString(),
+					player.getPositionVector(), Vec3d.ZERO,
+					0.7f, (float) (Math.random() / 4 + 0.75), 
+					(int) (Config.ConfigBalance.villagerConductorDistance * 1.2f), 
+					Gauge.STANDARD
+			);
+			ImmersiveRailroading.net.sendToAllAround(packet, new TargetPoint(player.dimension, player.posX, player.posY, player.posZ, Config.ConfigBalance.villagerConductorDistance * 1.2f));
+			
 			AxisAlignedBB bb = player.getEntityBoundingBox().grow(Config.ConfigBalance.villagerConductorDistance, 4, Config.ConfigBalance.villagerConductorDistance);
 			List<EntityCoupleableRollingStock> carsNearby = world.getEntitiesWithinAABB(EntityCoupleableRollingStock.class, bb);
 			EntityCoupleableRollingStock closestToPlayer = null;
