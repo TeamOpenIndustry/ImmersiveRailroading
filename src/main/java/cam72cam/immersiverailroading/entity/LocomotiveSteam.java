1699de9486d2291605d13a702b3ac313600e58df
Christian Mesh <christianmesh1@gmail.com>
2018-06-06 20:21:58 -0400

    Revert "And there we have a second Coder and his first addition ^^"
    This change does not satisfy the ticket, it still needs to damage
    players and entities around the locomotive.  It does nothing over the
    current explosion setting
    
    This reverts commit d37dcb91e4034fa6dd573c932b216cad5970cc69.

--- src/main/java/cam72cam/immersiverailroading/entity/LocomotiveSteam.java
+++ src/main/java/cam72cam/immersiverailroading/entity/LocomotiveSteam.java
@@ -606,13 +606,9 @@ public class LocomotiveSteam extends Locomotive {
 			// 10% over max pressure OR
 			// Half max pressure and high boiler temperature
 			//EXPLODE
-			if (Config.ConfigDamage.explosionsEnabled && Config.ConfigDamage.explosionDamageEnabled) {
+			if (Config.ConfigDamage.explosionsEnabled) {
 				for (int i = 0; i < 5; i++) {
 					world.createExplosion(this, this.posX, this.posY, this.posZ, boilerPressure/8, true);
-				}
-			} else if (Config.ConfigDamage.explosionsEnabled && Config.ConfigDamage.explosionDamageEnabled == false) {
-				for (int i = 0; i < 5; i++) {
-					world.createExplosion(this, this.posX, this.posY, this.posZ, 0, true);
 				}
 			}
 			world.removeEntity(this);
