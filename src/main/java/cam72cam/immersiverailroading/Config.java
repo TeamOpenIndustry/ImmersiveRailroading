2774070f98e3950606eae4c9ab9929c13cf0cad4
FriedrichLP <friedrichschirme@gmail.com>
2018-06-12 12:45:20 +0200

    Engines can now overheat! (Can be disabled in config)

--- src/main/java/cam72cam/immersiverailroading/Config.java
+++ src/main/java/cam72cam/immersiverailroading/Config.java
@@ -34,6 +34,9 @@ public class Config {
 
 		@Comment("Drop snowballs when the train can't push a block out of the way")
 		public static boolean dropSnowBalls = false;
+		
+		@Comment("Allow diesel locomotive engine overheating")
+		public static boolean canEnginesOverheat = true;
 	}
 
 	public static ConfigBalance balance;
