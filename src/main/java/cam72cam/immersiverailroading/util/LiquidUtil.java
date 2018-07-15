dfa9173392140a667ad628c3c77c289bf924e9ac
Christian Mesh <christianmesh1@gmail.com>
2018-07-15 12:29:01 -0400

    Forgot to add LiquidUtil

---
+++ src/main/java/cam72cam/immersiverailroading/util/LiquidUtil.java
@@ -0,0 +1,21 @@
+package cam72cam.immersiverailroading.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cam72cam.immersiverailroading.Config;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+
+public class LiquidUtil {
+	public static List<Fluid> getWater() {
+		List<Fluid> filter = new ArrayList<Fluid>();
+		filter.add(FluidRegistry.WATER);
+		for (String fluid : Config.ConfigBalance.waterTypes) {
+			if (FluidRegistry.getFluid(fluid) != null) {
+				filter.add(FluidRegistry.getFluid(fluid));
+			}
+		}
+		return filter;
+	}
+}
