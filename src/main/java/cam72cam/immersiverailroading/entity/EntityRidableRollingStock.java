c3eb4ad3644c81447268e9426ad396581614c910
Christian Mesh <christianmesh1@gmail.com>
2018-07-16 08:40:04 -0400

    Fix another stupid MC issue

--- src/main/java/cam72cam/immersiverailroading/entity/EntityRidableRollingStock.java
+++ src/main/java/cam72cam/immersiverailroading/entity/EntityRidableRollingStock.java
@@ -216,7 +216,9 @@ public abstract class EntityRidableRollingStock extends EntityBuildableRollingSt
 				pos = pos.subtract(0, 0.75, 0);
 			}
 			passenger.setPosition(pos.x, pos.y, pos.z);
-			passenger.setVelocity(this.motionX, this.motionY, this.motionZ);
+			passenger.motionX = this.motionX;
+			passenger.motionY = this.motionY;
+			passenger.motionZ = this.motionZ;
 			
 			passenger.prevRotationYaw = passenger.rotationYaw;
 			passenger.rotationYaw += (this.rotationYaw - this.prevRotationYaw);
