f95be8f83bb5fd6232b0952b74df81f4c7055da1
Christian Mesh <christianmesh1@gmail.com>
2018-08-01 17:46:48 -0400

    Version 1.3.2

--- src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
+++ src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class ImmersiveRailroading
 {
     public static final String MODID = "immersiverailroading";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
 	public static final int ENTITY_SYNC_DISTANCE = 512;
     
 	private static Logger logger;
