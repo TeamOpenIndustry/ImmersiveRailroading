1d8233bb1ff06724562af935aba1a4a62c440a64
EweLoHD <ewelo230@outlook.com>
2018-11-05 21:51:01 +0100

    rename?

--- src/main/java/cam72cam/immersiverailroading/items/ItemComponentsList.java
+++ src/main/java/cam72cam/immersiverailroading/items/ItemComponentsList.java
@@ -1,8 +1,13 @@
 package cam72cam.immersiverailroading.items;
 
 import cam72cam.immersiverailroading.ImmersiveRailroading;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 
 public class ItemComponentsList extends Item{
 	public static final String NAME = "item_components_list";
@@ -13,6 +18,11 @@ public class ItemComponentsList extends Item{
 		setUnlocalizedName(ImmersiveRailroading.MODID + ":" + NAME);
 		setRegistryName(new ResourceLocation(ImmersiveRailroading.MODID, NAME));
         this.setCreativeTab(ItemTabs.MAIN_TAB);
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		return super.onItemRightClick(world, player, hand);
 	}
 	
 }
