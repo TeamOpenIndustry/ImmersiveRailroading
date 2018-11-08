03acd43c4d242ed9bcf29a1580778e74b89076b2
EweLoHD <ewelo230@outlook.com>
2018-11-08 21:55:20 +0100

    gui now works

--- src/main/java/cam72cam/immersiverailroading/gui/ComponentsListGUI.java
+++ src/main/java/cam72cam/immersiverailroading/gui/ComponentsListGUI.java
@@ -1,15 +1,26 @@
 package cam72cam.immersiverailroading.gui;
 
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 import cam72cam.immersiverailroading.ImmersiveRailroading;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ComponentsListGUI extends GuiScreen{
 
 	private static ResourceLocation bookTexture = new ResourceLocation("immersiverailroading:gui/components_list.png");
-	
+	private final int bookImageHeight = 192;
+    private final int bookImageWidth = 192;
+    private static String[] stringPageText = new String[4];
+    private GuiButton buttonDone;
+    
+    
 	public ComponentsListGUI() {
 		
 	}
@@ -17,16 +28,27 @@ public class ComponentsListGUI extends GuiScreen{
 	@Override
 	public void initGui() {
 		System.out.println("initGUI");
+		int offsetFromScreenLeft = (width - bookImageWidth ) / 2;
+		int offsetFromScreenTop = (height - bookImageHeight ) / 2;
+		
+		buttonList.clear();
+        Keyboard.enableRepeatEvents(true);
+        
+        buttonDone = new GuiButton(0, offsetFromScreenLeft, offsetFromScreenTop + bookImageHeight + 4, bookImageWidth, 20, "Done");
+        buttonList.add(buttonDone);
 	}
 	
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		
-		System.out.println("drawScreen");
+		int offsetFromScreenLeft = (width - bookImageWidth ) / 2;
+		int offsetFromScreenTop = (height - bookImageHeight ) / 2;
 		
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		this.mc.getTextureManager().bindTexture(bookTexture);
+		drawTexturedModalRect(offsetFromScreenLeft, offsetFromScreenTop, 0, 0, bookImageWidth,  bookImageHeight);
 
+        Minecraft.getMinecraft().fontRenderer.drawString("test", offsetFromScreenLeft + 36, 32 + offsetFromScreenTop, 0xFFFFFF);
+		
 		super.drawScreen(mouseX, mouseY, partialTicks);
 	}
 	
@@ -34,5 +56,6 @@ public class ComponentsListGUI extends GuiScreen{
 	public boolean doesGuiPauseGame() {
 		return false;
 	}
+	
 	
 }
