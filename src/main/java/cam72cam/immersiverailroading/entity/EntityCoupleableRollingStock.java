TeamOpenIndustry/ImmersiveRailroading

commit 4ee7d256bb9837479a36a0f18d085bc4593e76a4

Clean up some NBT hacks

src/main/java/cam72cam/immersiverailroading/entity/EntityCoupleableRollingStock.java
@@ -11,6 +11,7 @@ import java.util.function.Function;
 
 import javax.annotation.Nullable;
 
+import net.minecraft.nbt.NBTBase;
 import org.apache.commons.lang3.tuple.Pair;
 
 import com.google.common.base.Predicate;
@@ -102,14 +103,14 @@ public abstract class EntityCoupleableRollingStock extends EntityMoveableRolling
 		if (coupledFront != null) {
 			nbttagcompound.setString("CoupledFront", coupledFront.toString());
 			if (lastKnownFront != null) {
-				nbttagcompound.setTag("lastKnownFront", NBTUtil.blockPosToNBT(lastKnownFront));
+				nbttagcompound.setLong("lastKnownFront", lastKnownFront.toLong());
 			}
 		}
 		nbttagcompound.setBoolean("frontCouplerEngaged", frontCouplerEngaged);
 		if (coupledBack != null) {
 			nbttagcompound.setString("CoupledBack", coupledBack.toString());
 			if (lastKnownRear != null) {
-				nbttagcompound.setTag("lastKnownRear", NBTUtil.blockPosToNBT(lastKnownRear));
+				nbttagcompound.setLong("lastKnownRear", lastKnownRear.toLong());
 			}
 		}
 		nbttagcompound.setBoolean("backCouplerEngaged", backCouplerEngaged);
@@ -121,15 +122,26 @@ public abstract class EntityCoupleableRollingStock extends EntityMoveableRolling
 		if (nbttagcompound.hasKey("CoupledFront")) {
 			coupledFront = UUID.fromString(nbttagcompound.getString("CoupledFront"));
 			if (nbttagcompound.hasKey("lastKnownFront")) {
-				lastKnownFront = NBTUtil.nbtToBlockPos(nbttagcompound.getCompoundTag("lastKnownFront"));
+				if (nbttagcompound.getTag("lastKnownFront").getId() == 10) {
+					// Legacy
+					// TODO remove 2.0
+					NBTTagCompound pos = nbttagcompound.getCompoundTag("lastKnownFront");
+					lastKnownFront = new BlockPos(pos.getInteger("x"), pos.getInteger("y"), pos.getInteger("z"));
+				} else {
+					lastKnownFront = BlockPos.fromLong(nbttagcompound.getLong("lastKnownFront"));
+				}
 			}
 		}
 		frontCouplerEngaged = nbttagcompound.getBoolean("frontCouplerEngaged");
 
 		if (nbttagcompound.hasKey("CoupledBack")) {
-			coupledBack = UUID.fromString(nbttagcompound.getString("CoupledBack"));
-			if (nbttagcompound.hasKey("lastKnownRear")) {
-				lastKnownRear = NBTUtil.nbtToBlockPos(nbttagcompound.getCompoundTag("lastKnownRear"));
+			if (nbttagcompound.getTag("lastKnownRear").getId() == 10) {
+				// Legacy
+				// TODO remove 2.0
+				NBTTagCompound pos = nbttagcompound.getCompoundTag("lastKnownRear");
+				lastKnownRear = new BlockPos(pos.getInteger("x"), pos.getInteger("y"), pos.getInteger("z"));
+			} else {
+				lastKnownRear = BlockPos.fromLong(nbttagcompound.getLong("lastKnownRear"));
 			}
 		}
 		backCouplerEngaged = nbttagcompound.getBoolean("backCouplerEngaged");

src/main/java/cam72cam/immersiverailroading/tile/TileRail.java
@@ -105,7 +105,6 @@ public class TileRail extends TileRailBase {
 			nbt.setTag("placementPosition", newPositionFormat);
 
             PlacementInfo placementInfo = new PlacementInfo(nbt, pos);
-            System.out.println(placementInfo.toNBT());
             placementInfo = new PlacementInfo(placementInfo.placementPosition, placementInfo.rotationQuarter, placementInfo.direction, placementInfo.facing.getOpposite());
 
 			SwitchState switchState = SwitchState.values()[nbt.getInteger("switchState")];

src/main/java/cam72cam/immersiverailroading/tile/TileRailBase.java
@@ -160,8 +160,8 @@ public class TileRailBase extends SyncdTileEntity implements ITrack, ITickable {
 
 	public BlockPos getParent() {
 		if (parent == null) {
-			ImmersiveRailroading.warn("Invalid block without parent");
 			if (ticksExisted > 1 && !world.isRemote) {
+				ImmersiveRailroading.warn("Invalid block without parent");
 				// Might be null during init
 				world.setBlockToAir(pos);
 			}
@@ -237,19 +237,20 @@ public class TileRailBase extends SyncdTileEntity implements ITrack, ITickable {
 		if (nbt.hasKey("augment")) {
 			augment = Augment.values()[nbt.getInteger("augment")];
 		}
-		
+
+		parent = BlockPos.fromLong(nbt.getLong("parent"));
+
 		switch(version) {
 		case 0:
 			//NOP
 		case 1:
-			setNBTBlockPos(nbt, "parent", getNBTBlockPos(nbt, "parent").subtract(pos));
+			parent = parent.subtract(pos);
 		case 2:
 			// Nothing in base
 		case 3:
 			// Nothing yet ...
 		}
-		parent = getNBTBlockPos(nbt, "parent");
-		
+
 		if (nbt.hasKey("augmentTank")) {
 			createAugmentTank();
 			augmentTank.readFromNBT(nbt.getCompoundTag("augmentTank"));			
@@ -272,7 +273,7 @@ public class TileRailBase extends SyncdTileEntity implements ITrack, ITickable {
 	}
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
-		setNBTBlockPos(nbt, "parent", parent);
+		nbt.setLong("parent", parent.toLong());
 		nbt.setFloat("height", bedHeight);
 		nbt.setFloat("railHeight", railHeight);
 		nbt.setInteger("snowLayers", snowLayers);
@@ -300,32 +301,6 @@ public class TileRailBase extends SyncdTileEntity implements ITrack, ITickable {
 		return super.writeToNBT(nbt);
 	}
 	
-	protected final static void setNBTBlockPos(NBTTagCompound nbt, String key, BlockPos value) {
-		if (value != null) {
-			nbt.setLong(key, value.toLong());
-		}
-	}
-	protected final static void setNBTVec3d(NBTTagCompound nbt, String key, Vec3d value) {
-		if (value != null) {
-			nbt.setDouble(key + "X", value.x);
-			nbt.setDouble(key + "Y", value.y);
-			nbt.setDouble(key + "Z", value.z);
-		}
-	}
-	
-	protected final static BlockPos getNBTBlockPos(NBTTagCompound nbt, String key) {
-		return nbt.hasKey(key) ? BlockPos.fromLong(nbt.getLong(key)) : null;
-	}
-	protected final static Vec3d getNBTVec3d(NBTTagCompound nbt, String key) {
-		if (!nbt.hasKey(key + "X") || !nbt.hasKey(key + "Y") || !nbt.hasKey(key + "Z")) {
-			return null;
-		}
-		return new Vec3d(nbt.getDouble(key + "X"),nbt.getDouble(key + "Y"),nbt.getDouble(key + "Z"));
-	}
-	
-	public Vec3d getCenterOfRail() {
-		return new Vec3d(this.getPos()).addVector(0.5, 0, 0.5);
-	}
 	public TileRail getParentTile() {
 		if (this.getParent() == null) {
 			return null;
@@ -341,10 +316,6 @@ public class TileRailBase extends SyncdTileEntity implements ITrack, ITickable {
 	}
 	public NBTTagCompound getReplaced() {
 		return replaced;
-	}
-	
-	public void setSkipNextRefresh() {
-		this.skipNextRefresh = true;
 	}
 	
 	@Override

src/main/java/cam72cam/immersiverailroading/util/NBTUtil.java
@@ -6,19 +6,7 @@ import net.minecraft.util.math.Vec3d;
 
 public class NBTUtil {
 	//TODO move other nbt helpers here
-	
-	public static BlockPos nbtToBlockPos(NBTTagCompound nbt) {
-		return new BlockPos(nbt.getInteger("x"), nbt.getInteger("y"), nbt.getInteger("z"));
-	}
-	
-	public static NBTTagCompound blockPosToNBT(BlockPos pos) {
-		NBTTagCompound nbt = new NBTTagCompound();
-		nbt.setInteger("x", pos.getX());
-		nbt.setInteger("y", pos.getY());
-		nbt.setInteger("z", pos.getZ());
-		return nbt;
-	}
-	
+
 	public final static NBTTagCompound vec3dToNBT(Vec3d value) {
 		NBTTagCompound nbt = new NBTTagCompound();
 		if (value != null) {

commit 881aea506ae7a7c1cc17b113184b650bddabe6ce

Re-add coupledBack loading (broken in 4ee7d256bb9837479a36a0f18d085bc4593e76a4)

src/main/java/cam72cam/immersiverailroading/entity/EntityCoupleableRollingStock.java
@@ -135,6 +135,7 @@ public abstract class EntityCoupleableRollingStock extends EntityMoveableRolling
 		frontCouplerEngaged = nbttagcompound.getBoolean("frontCouplerEngaged");
 
 		if (nbttagcompound.hasKey("CoupledBack")) {
+			coupledBack = UUID.fromString(nbttagcompound.getString("CoupledBack"));
 			if (nbttagcompound.getTag("lastKnownRear").getId() == 10) {
 				// Legacy
 				// TODO remove 2.0