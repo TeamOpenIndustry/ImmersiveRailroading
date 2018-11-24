bdcc648855472666ad1eb52bad6d70db65af4321
Christian Mesh <christianmesh1@gmail.com>
2018-11-24 15:02:01 -0500

    Fix slopes from straight update

--- src/main/java/cam72cam/immersiverailroading/track/BuilderSlope.java
+++ src/main/java/cam72cam/immersiverailroading/track/BuilderSlope.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import cam72cam.immersiverailroading.util.RailInfo;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 
 public class BuilderSlope extends BuilderStraight {
 	public BuilderSlope(RailInfo info, BlockPos pos) {
@@ -15,12 +16,10 @@ public class BuilderSlope extends BuilderStraight {
 		List<PosStep> res = new ArrayList<PosStep>();
 		
 		float slope = (1.0F/(info.settings.length));
-		
-		float height = slope/2;
-		
+		float startHeight = slope/2;
+
 		for (PosStep cur : super.getPath(stepSize)) {
-			res.add(new PosStep(cur.x, cur.y + height, cur.z, cur.yaw));
-			height += slope * stepSize;
+			res.add(new PosStep(cur.x, cur.y + startHeight + slope * cur.distanceTo(Vec3d.ZERO), cur.z, cur.yaw));
 		}
 		
 		return res;
