b985e9e2dfd27c8b9484e59f3f55b5baf4abd409
Christian Mesh <christianmesh1@gmail.com>
2018-11-24 14:25:40 -0500

    Allow altering settings using track placement tool

--- src/main/java/cam72cam/immersiverailroading/items/nbt/RailSettings.java
+++ src/main/java/cam72cam/immersiverailroading/items/nbt/RailSettings.java
@@ -72,4 +72,19 @@ public class RailSettings {
         nbt.setBoolean("isGradeCrossing", isGradeCrossing);
         return nbt;
     }
+
+    public RailSettings withLength(int length) {
+       return new RailSettings(
+				gauge,
+				type,
+				length,
+				quarters,
+				posType,
+				direction,
+				railBed,
+				railBedFill,
+				isPreview,
+				isGradeCrossing
+		) ;
+    }
 }
