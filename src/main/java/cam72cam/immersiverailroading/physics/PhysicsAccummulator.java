26eb3645dc0bdc37d58424cd9e85a0cca9435692
Brendan Thomas <bmtmail@comcast.net>
2018-12-30 22:51:58 -0800

    Air resistance approximation
    Added a simulation for air resistance. This effect is generally quite small except on short, light, or very fast moving trains. For example, a 10 car fully loaded frieght train running at 95 km/hr had a wind resistance about 40% of its rolling resistance.
    
    This simulation makes a lot of assumptions due to lack of information. In particular, it assumes all cars of the train have the same cross-sectional area in the same shape. In a train with varying cars, the difference in cross section from one car to the next would increase air resistance. In trains with smaller cars, air resistance would be proportionally smaller. Overall I think the approximation is within a factor of 2 of the real value in most cases.

--- src/main/java/cam72cam/immersiverailroading/physics/PhysicsAccummulator.java
+++ src/main/java/cam72cam/immersiverailroading/physics/PhysicsAccummulator.java
@@ -68,6 +68,7 @@ public class PhysicsAccummulator {
 
 	//gets velocity in m/t for 1 tick (1/20 second) of recorded acceleration forces
 	public Speed getVelocity() {
+		double currentMCVelocity = pos.speed.minecraft();
 		double tickRatio = 1.0 / 400.0; // second^2 per tick^2
 
 		// Note, brake strength (pad to wheel) is actually independent of wheel adhesion (wheel to rails)
@@ -75,14 +76,26 @@ public class PhysicsAccummulator {
 		// Implementing over-braking is probably too immersive even for this mod
 		// Note, this assumption means brakes are very powerful in game, roughly 2x better than real life
 		double brakeForceNewtons = wheelAdhesionNewtons * airBrakeEffect * Config.ConfigBalance.brakeMultiplier;
-		
+
 		// a = f (to newtons) * m (to newtons)
 		double deltaAccellTractiveMCVelocity = tractiveEffortNewtons / massToMoveKg * tickRatio;
 		double deltaAccellRollingResistanceMCVelocity = rollingResistanceNewtons / massToMoveKg * tickRatio;
 		double deltaAccellGradeMCVelocity = gradeForceNewtons / massToMoveKg * tickRatio; //implied * 1 for 1 tick per call
 		double deltaAccellBrakeMCVelocity = brakeForceNewtons / massToMoveKg * tickRatio; //N / kg * s^2 / t^2 * t = m / t
-		
-		double currentMCVelocity = pos.speed.minecraft();
+
+		// no wind resistance is current;y given on stock definitions, so we approximate
+		// http://eprints.whiterose.ac.uk/87909/1/The%20aerodynamics%20of%20freight_WRRO.pdf
+		// wind force = coefficient * area * 1/2 * air pressure (~1.2 kg/m^3) * velocity^2
+		double assumedStockFactor = .005; // coefficient
+
+		// the parts of the equation shared per type (area, air pressure, velocity)
+		double commonWindFactor = 10 * .5 * 1.2 * Math.pow(currentMCVelocity * 20, 2);// velocity * 20 for m/t -> m/s
+
+		//the front car is fully, exposed, so it has a higher coefficient
+		//the coefficient of a flat surface is ~2.1, we assume half that for the front train because most trains are blocky
+		double windFrontNewtons = 1.05 * commonWindFactor; //velocity * 20 for m/t -> m/s
+		double windOtherNewtons = assumedStockFactor * commonWindFactor * (count - 1); //count - 1 cars behind front car
+		double deltaAccellWindMCVelocity = (windFrontNewtons + windOtherNewtons) / massToMoveKg * tickRatio;
 
 		// tractive acceleration and grade are always applied
 		// technically tractive force and braking force should be summed and capped to wheel adhesion
@@ -92,8 +105,8 @@ public class PhysicsAccummulator {
 
 		// friction and brakes are limited to stopping
 		// Apply in the reverse direction of current travel
-		double deltaDecell = -1 * Math.copySign(Math.min(deltaAccellRollingResistanceMCVelocity +  deltaAccellBrakeMCVelocity, Math.abs(newMCVelocity)), newMCVelocity);
-		
+		double deltaDecell = -1 * Math.copySign(Math.min(deltaAccellRollingResistanceMCVelocity +
+				deltaAccellBrakeMCVelocity + deltaAccellWindMCVelocity, Math.abs(newMCVelocity)), newMCVelocity);
 		newMCVelocity = newMCVelocity + deltaDecell;
 
 		if (Math.abs(newMCVelocity) < PhysicsAccummulator.MIN_SPEED) {
