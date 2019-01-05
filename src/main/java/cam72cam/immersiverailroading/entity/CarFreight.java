39cff717898e8f29d1970723e890b5f913d9845b
Christian Mesh <christianmesh1@gmail.com>
2019-01-05 08:23:08 -0500

    Revert part of 73f3cdd50, Legend committed broken code to master

--- src/main/java/cam72cam/immersiverailroading/entity/CarFreight.java
+++ src/main/java/cam72cam/immersiverailroading/entity/CarFreight.java
@@ -1,14 +1,10 @@
 package cam72cam.immersiverailroading.entity;
 
-import cam72cam.immersiverailroading.ImmersiveRailroading;
 import cam72cam.immersiverailroading.registry.CarFreightDefinition;
-import cam72cam.immersiverailroading.sound.ISound;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 
 public class CarFreight extends Freight {
-	private ISound squealsound;
-	
+
 	public CarFreight(World world) {
 		this(world, null);
 	}
@@ -30,18 +26,6 @@ public class CarFreight extends Freight {
 	public int getInventoryWidth() {
 		return this.getDefinition().getInventoryWidth(gauge);
 	}
-	@Override
-	public void onUpdate() {
-		super.onUpdate();	
-		
-		if(this.getCurrentSpeed().metric() >= 35) {
-			if (this.squealsound == null) {
-				squealsound = ImmersiveRailroading.proxy.newSound(new ResourceLocation("immersiverailroading:sounds/misc/squeal_1.ogg"), true, 40, gauge);
-				squealsound.setPitch((float) (0.2*this.getCurrentSpeed().metric()));
-			}
-						
-		}
-	}
-	
+
 	//TODO filter inventory
 }
