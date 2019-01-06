0516b441519d9d252fca7dc5179758e56cdde8d8
Christian Mesh <christianmesh1@gmail.com>
2019-01-06 10:47:38 -0500

    Bump version to 1.5.0

--- src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
+++ src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class ImmersiveRailroading
 {
     public static final String MODID = "immersiverailroading";
-    public static final String VERSION = "1.4.1";
+    public static final String VERSION = "1.5.0";
 	public static final int ENTITY_SYNC_DISTANCE = 512;
     
 	private static Logger logger;
