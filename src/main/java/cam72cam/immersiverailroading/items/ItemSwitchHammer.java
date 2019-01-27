61f1df840c78152d878a5ea24c01c97be33c1564
Jacob Hayes <tigerbird2@gmail.com>
2019-01-27 17:23:48 -0500

    Name and location refactors

--- src/main/java/cam72cam/immersiverailroading/items/ItemSwitchHammer.java
+++ src/main/java/cam72cam/immersiverailroading/items/ItemSwitchHammer.java
@@ -14,7 +14,7 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 public class ItemSwitchHammer extends Item {
-	public static final String NAME = "item_switch_hammer";
+	public static final String NAME = "item_switch_key";
 
 	public ItemSwitchHammer() {
 		super();
