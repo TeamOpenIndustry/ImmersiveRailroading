5b6eb05bab85f1f6acdd326a518f57bedca90e48
Christian Mesh <christianmesh1@gmail.com>
2019-04-21 08:05:26 -0400

    Update check_model.sh to find missing texture files

--- check_models.sh
+++ check_models.sh
@@ -1,12 +1,19 @@
 for mtl in $(find src/main/resources/assets/immersiverailroading/models/rolling_stock/ | grep "mtl$"); do
 	echo $mtl
 	dir=$(dirname $mtl)
-	data=$(cat $mtl | grep map_Kd | sed -e 's/map_Kd // ')
+	kdmap=$(cat $mtl | grep map_Kd | sed -e 's/map_Kd // ')
 	for fpath in $(find $dir | grep png); do
 		fname=$(basename $fpath)
-		if [[ $data != *"$fname"* ]]; then
+		if [[ $kdmap != *"$fname"* ]]; then
 			echo "$fname UNUSED"
 			rm -rf $fpath
+		fi
+	done
+
+	pngmap=$(find $dir | grep png)
+	for fname in $(cat $mtl | grep map_Kd | sed -e 's/map_Kd // '); do
+		if [[ $pngmap != *"$fname"* ]]; then
+			echo "$fname MISSING"
 		fi
 	done
 done
