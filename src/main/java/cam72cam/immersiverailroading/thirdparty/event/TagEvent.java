634da719bba824c66fa3d5d6faa81c5c44beda46
CSX8600 <cnwaj@hotmail.com>
2019-06-17 22:08:08 -0500

    Include stock persistent ID in TagEvent

--- src/main/java/cam72cam/immersiverailroading/thirdparty/event/TagEvent.java
+++ src/main/java/cam72cam/immersiverailroading/thirdparty/event/TagEvent.java
@@ -1,19 +1,29 @@
 package cam72cam.immersiverailroading.thirdparty.event;
 
+import java.util.UUID;
+
 import net.minecraftforge.fml.common.eventhandler.Event;
 
 public class TagEvent extends Event {
 	private EventType eventType;
-	public TagEvent(EventType eventType)
+	private UUID stockID;
+	public TagEvent(EventType eventType, UUID stockID)
 	{
 		super();
 		this.eventType = eventType;
+		this.stockID = stockID;
 	}
 	
 	public String tag;
+	
 	public EventType getEventType()
 	{
 		return eventType;
+	}
+	
+	public UUID getStockID()
+	{
+		return stockID;
 	}
 	
 	public enum EventType
