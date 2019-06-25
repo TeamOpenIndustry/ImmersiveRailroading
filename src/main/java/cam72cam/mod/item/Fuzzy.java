e91bb866d7815bd628faa20649526017b4606456
Christian Mesh <christianmesh1@gmail.com>
2019-06-25 11:35:42 -0400

    Some more item fuzzy logic

--- src/main/java/cam72cam/mod/item/Fuzzy.java
+++ src/main/java/cam72cam/mod/item/Fuzzy.java
@@ -1,14 +1,24 @@
 package cam72cam.mod.item;
 
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class Fuzzy {
     public static final Fuzzy WOOD_PLANK = new Fuzzy("plankWood");
+    public static final Fuzzy REDSTONE_DUST = new Fuzzy("dustRedstone");
+    public static final Fuzzy SNOW_LAYER = new Fuzzy("layerSnow", Blocks.SNOW_LAYER);
+    public static final Fuzzy SNOW_BLOCK = new Fuzzy("blockSnow", Blocks.SNOW);
 
     private final String ident;
 
     public Fuzzy(String ident) {
         this.ident = ident;
+    }
+
+    public Fuzzy(String ident, Block block) {
+        this(ident);
+        OreDictionary.registerOre(ident, block);
     }
 
     public boolean matches(ItemStack stack) {
