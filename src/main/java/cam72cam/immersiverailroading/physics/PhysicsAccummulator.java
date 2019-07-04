e398633aa9b73cb2c751ce8b857a333ae6532593
Brendan Thomas <bmtmail@comcast.net>
2019-07-04 01:24:02 -0700

    Prevent concurrent modification
    Calling getCurrentTickPosAndPrune() could potentially remove elements from the stock's tickPos list while it was being iterated. So now get tickPos without pruning.

--- src/main/java/cam72cam/immersiverailroading/physics/PhysicsAccummulator.java
+++ src/main/java/cam72cam/immersiverailroading/physics/PhysicsAccummulator.java
@@ -45,7 +45,12 @@ public class PhysicsAccummulator {
 
 		// see http://hyperphysics.phy-astr.gsu.edu/hbase/mincl.html for slope physics
 		// negative because a positive rotationPitch means stock is pointing uphill
-		double gradeForceFactor = -Math.sin(Math.toRadians(movable.getCurrentTickPosAndPrune().rotationPitch)) * Config.ConfigBalance.slopeMultiplier;
+		TickPos posToUse = movable.getTickPos((int) movable.tickPosID);
+		if(posToUse == null)
+		{
+			posToUse = pos;
+		}
+		double gradeForceFactor = -Math.sin(Math.toRadians(posToUse.rotationPitch)) * Config.ConfigBalance.slopeMultiplier;
 		gradeForceNewtons += stock.getWeight() * gradeForceFactor * 9.8;
 		
 		if (stock instanceof Locomotive) {
