27b628a8502c2130818320243e53f66c7f44e479
Christian Mesh <christianmesh1@gmail.com>
2019-07-11 08:18:44 -0400

    mostly working track placement

--- src/main/java/cam72cam/mod/block/BlockTypeTickable.java
+++ src/main/java/cam72cam/mod/block/BlockTypeTickable.java
@@ -12,6 +12,6 @@ public class BlockTypeTickable extends BlockTypeEntity {
 
     @Override
     protected TileEntity getTile() {
-        return new TileEntityTickable();
+        return new TileEntityTickable(id);
     }
 }
