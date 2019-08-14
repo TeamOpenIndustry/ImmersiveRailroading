b5152031a9f4a4ea47d7dee5e6bb9a9f42df3017
Christian Mesh <christianmesh1@gmail.com>
2019-08-14 19:46:54 -0400

    Convert MBBlueprintRender to StandardModel

--- src/main/java/cam72cam/immersiverailroading/render/multiblock/MBBlueprintRender.java
+++ src/main/java/cam72cam/immersiverailroading/render/multiblock/MBBlueprintRender.java
@@ -3,28 +3,22 @@ package cam72cam.immersiverailroading.render.multiblock;
 import cam72cam.immersiverailroading.multiblock.Multiblock;
 import cam72cam.immersiverailroading.multiblock.MultiblockRegistry;
 import cam72cam.mod.item.ItemStack;
+import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.math.Vec3i;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.RenderItem;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
-import org.lwjgl.opengl.GL11;
+import cam72cam.mod.render.StandardModel;
 
 import java.util.Map;
 
 public class MBBlueprintRender {
-	private static RenderItem render;
-
 	public static void draw(String name) {
-		if (render == null) {
-			render = Minecraft.getMinecraft().getRenderItem();
-		}
-		
 		Multiblock mb = MultiblockRegistry.get(name);
 		if (mb == null) {
 			// Some wrappers (Akashic Tome) remove the metadata
 			return;
 		}
+		float scale = 0.8f;
 		Map<Vec3i, ItemStack> bp = mb.blueprint();
+        StandardModel model = new StandardModel();
         for (Vec3i pos : bp.keySet()) {
 
             ItemStack state = bp.get(pos);
@@ -33,9 +27,8 @@ public class MBBlueprintRender {
             }
 
             Vec3i rPos = pos.subtract(mb.placementPos());
-            GL11.glTranslated(rPos.x, rPos.y, rPos.z);
-            render.renderItem(state.internal, ItemCameraTransforms.TransformType.NONE);
-            GL11.glTranslated(-rPos.x, -rPos.y, -rPos.z);
+            model.addItem(state, new Vec3d(rPos.x, rPos.y, rPos.z), new Vec3d(scale,scale,scale));
         }
+        model.render();
 	}
 }
