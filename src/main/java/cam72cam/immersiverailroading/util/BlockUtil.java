bfb0ee05bc651b22f09f1eb659abb5ca6bd0df56
Christian Mesh <christianmesh1@gmail.com>
2019-08-15 08:00:07 -0400

    Remove unused function

--- src/main/java/cam72cam/immersiverailroading/util/BlockUtil.java
+++ src/main/java/cam72cam/immersiverailroading/util/BlockUtil.java
@@ -4,9 +4,6 @@ import cam72cam.immersiverailroading.IRBlocks;
 import cam72cam.immersiverailroading.tile.RailBase;
 import cam72cam.mod.math.Vec3i;
 import cam72cam.mod.world.World;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockLog;
-import net.minecraft.block.state.IBlockState;
 import trackapi.lib.Util;
 
 public class BlockUtil {
@@ -24,17 +21,7 @@ public class BlockUtil {
 		}
 		return false;
 	}
-	
-	public static IBlockState itemToBlockState(cam72cam.mod.item.ItemStack stack) {
-		Block block = Block.getBlockFromItem(stack.internal.getItem());
-		@SuppressWarnings("deprecation")
-		IBlockState gravelState = block.getStateFromMeta(stack.internal.getMetadata());
-		if (block instanceof BlockLog ) {
-			gravelState = gravelState.withProperty(BlockLog.LOG_AXIS, BlockLog.EnumAxis.Z);
-		}
-		return gravelState;
-	}
-	
+
 	public static boolean isIRRail(World world, Vec3i pos) {
 		return world.isBlock(pos, IRBlocks.BLOCK_RAIL_GAG) || world.isBlock(pos, IRBlocks.BLOCK_RAIL);
 	}
