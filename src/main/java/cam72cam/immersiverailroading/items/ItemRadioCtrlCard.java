23b15697f34889cd3658466c0121da79eccf4fd8
Christian Mesh <christianmesh1@gmail.com>
2019-09-16 21:00:03 -0400

    Add programatic radio control card recipe

--- src/main/java/cam72cam/immersiverailroading/items/ItemRadioCtrlCard.java
+++ src/main/java/cam72cam/immersiverailroading/items/ItemRadioCtrlCard.java
@@ -1,14 +1,23 @@
 package cam72cam.immersiverailroading.items;
 
-import java.util.List;
-
 import cam72cam.immersiverailroading.ImmersiveRailroading;
+import cam72cam.mod.item.Fuzzy;
 import cam72cam.mod.item.ItemBase;
 import cam72cam.mod.item.ItemStack;
+import cam72cam.mod.item.Recipes;
+
+import java.util.List;
 
 public class ItemRadioCtrlCard extends ItemBase {
     public ItemRadioCtrlCard() {
         super(ImmersiveRailroading.MODID, "item_radio_control_card", 1, ItemTabs.MAIN_TAB);
+        Fuzzy transistor = new Fuzzy("oc:materialTransistor");
+        Fuzzy dataCard = new Fuzzy("oc:dataCard1");
+        Recipes.register(this, 3,
+                null, Fuzzy.IRON_BARS, null,
+                transistor, Fuzzy.IRON_INGOT, transistor,
+                null, dataCard, null
+        );
     }
 
     @Override
