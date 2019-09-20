ed316995b76c57023286122921e0258bcf7db600
Christian Mesh <christianmesh1@gmail.com>
2019-09-20 07:19:53 -0400

    Move modcore to a different repo

--- src/main/java/cam72cam/mod/block/BlockTypeTickable.java
+++
@@ -1,9 +0,0 @@
-package cam72cam.mod.block;
-
-import java.util.function.Supplier;
-
-public class BlockTypeTickable extends BlockTypeEntity {
-    public BlockTypeTickable(BlockSettings settings, Supplier<BlockEntityTickable> constructData) {
-        super(settings, constructData::get);
-    }
-}
