4ffcb9c2c847e6f0d77d31039dc4f23bb16ff9d2
Christian Mesh <christianmesh1@gmail.com>
2019-09-21 09:25:54 -0400

    Update setup to take branch param

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
+branch=$1
+
+if [ -z "$branch" ]; then
+		branch="forge_1.12.2"
+fi
+
 unlink $PWD/src/main/java/cam72cam/immersiverailroading/thirdparty
 
 rm -rf ./ImmersiveRailroadingMod
 rm -rf ./ImmersiveRailroadingIntegration
 
-git clone git@github.com:cam72cam/ImmersiveRailroadingIntegration.git
-git clone git@github.com:cam72cam/ModCoreMod.git ImmersiveRailroadingMod
+git clone --branch $branch git@github.com:cam72cam/ImmersiveRailroadingIntegration.git
+git clone --branch $branch git@github.com:cam72cam/ModCoreMod.git ImmersiveRailroadingMod
 
 ln -s $PWD/src/main/java/cam72cam/immersiverailroading $PWD/ImmersiveRailroadingMod/src/main/java/cam72cam/immersiverailroading
 ln -s $PWD/ImmersiveRailroadingIntegration/src/main/java/cam72cam/immersiverailroading/thirdparty $PWD/ImmersiveRailroadingMod/src/main/java/cam72cam/immersiverailroading/thirdparty
 ln -s $PWD/src/main/resources $PWD/ImmersiveRailroadingMod/src/main/resources
 
 pushd ImmersiveRailroadingMod
-		bash setup.sh immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
+		bash setup.sh $branch immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
 		echo "apply from: '../ImmersiveRailroadingIntegration/dependencies.gradle'" >> build.gradle
 popd
