6c300bfb320a1374d3a12e7e3c45cdf7145944f9
Christian Mesh <christianmesh1@gmail.com>
2019-09-28 10:02:07 -0400

    Update powershell script

--- setup.ps1
+++ setup.ps1
@@ -6,21 +6,13 @@ if(Test-Path -Path $PWD/src/main/java/cam72cam/immersiverailroading/thirdparty )
 		Remove-Item $PWD/src/main/java/cam72cam/immersiverailroading/thirdparty
 }
 
-Remove-Item -Recurse -Force ./ImmersiveRailroadingMod
+Remove-Item -Recurse -Force ./TrackAPI
+Remove-Item -Recurse -Force ./UniversalModCore
 Remove-Item -Recurse -Force ./ImmersiveRailroadingIntegration
 
-if(!(Test-Path -Path run )){
-		New-Item -ItemType directory run
-}
-
+git clone --branch $branch git@github.com:TeamOpenIndustry/TrackAPI.git
+git clone --branch $branch git@github.com:TeamOpenIndustry/UniversalModCore.git
 git clone --branch $branch git@github.com:TeamOpenIndustry/ImmersiveRailroadingIntegration.git
-git clone --branch $branch git@github.com:TeamOpenIndustry/UniversalModCoreMod.git ImmersiveRailroadingMod
 
-New-Item -ItemType SymbolicLink -Target $PWD/src/main/java/cam72cam/immersiverailroading -Path $PWD/ImmersiveRailroadingMod/src/main/java/cam72cam/immersiverailroading
-New-Item -ItemType SymbolicLink -Target $PWD/ImmersiveRailroadingIntegration/src/main/java/cam72cam/immersiverailroading/thirdparty -Path $PWD/ImmersiveRailroadingMod/src/main/java/cam72cam/immersiverailroading/thirdparty
-New-Item -ItemType SymbolicLink -Target $PWD/src/main/resources -Path $PWD/ImmersiveRailroadingMod/src/main/resources
-New-Item -ItemType SymbolicLink -Target $PWD/run -Path $PWD/ImmersiveRailroadingMod/run
-
-cd ImmersiveRailroadingMod
-./setup.ps1 -branch $branch immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
-Add-Content -Path build.gradle -Value "apply from: '../ImmersiveRailroadingIntegration/dependencies.gradle'"
+./UniversalModCore/template/setup.ps1 $branch immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
+(Get-Content build.gradle) -replace '^dependencies {', "apply from: 'ImmersiveRailroadingIntegration/dependencies.gradle'\ndependencies {" | Set-Content build.gradle 
--- setup.sh
+++ setup.sh
@@ -8,11 +8,9 @@ rm -rf ./TrackAPI
 rm -rf ./UniversalModCore
 rm -rf ./ImmersiveRailroadingIntegration
 
-mkdir -p run
-
 git clone --branch $branch git@github.com:TeamOpenIndustry/TrackAPI.git
 git clone --branch $branch git@github.com:TeamOpenIndustry/UniversalModCore.git
 git clone --branch $branch git@github.com:TeamOpenIndustry/ImmersiveRailroadingIntegration.git
 
-bash ./UniversalModCore/template/setup.sh $branch immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
+./UniversalModCore/template/setup.sh $branch immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
 sed -i build.gradle -e "s,^dependencies {,apply from: 'ImmersiveRailroadingIntegration/dependencies.gradle'\n\0,"
