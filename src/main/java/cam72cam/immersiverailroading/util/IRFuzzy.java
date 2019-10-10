634a6a03985bb04d515e57ae39976bb15942d02c
Christian Mesh <christianmesh1@gmail.com>
2019-10-10 08:48:39 -0400

    Fix IRFuzzy fallback

--- src/main/java/cam72cam/immersiverailroading/util/IRFuzzy.java
+++ src/main/java/cam72cam/immersiverailroading/util/IRFuzzy.java
@@ -24,28 +24,31 @@ public class IRFuzzy {
             .addAll(Fuzzy.WOOD_PLANK);
 
     public static void applyFallbacks() {
-        if (IR_CASTING_CASING.example() == null) {
+        if (IR_CASTING_CASING.example().isEmpty()) {
             IR_CASTING_CASING.addAll(Fuzzy.NETHER_BRICK);
         }
-        if (IR_LIGHT_ENG.example() == null) {
+        if (IR_LIGHT_ENG.example().isEmpty()) {
             IR_LIGHT_ENG.addAll(Fuzzy.IRON_BLOCK);
         }
-        if (IR_HEAVY_ENG.example() == null) {
+        if (IR_HEAVY_ENG.example().isEmpty()) {
             IR_HEAVY_ENG.addAll(Fuzzy.IRON_BLOCK);
         }
-        if (IR_SCAFFOLDING.example() == null) {
+        if (IR_SCAFFOLDING.example().isEmpty()) {
             IR_SCAFFOLDING.addAll(Fuzzy.IRON_BARS);
         }
-        if (IR_STEEL_INGOT.example() == null) {
+        if (IR_STEEL_INGOT.example().isEmpty()) {
             IR_STEEL_INGOT.addAll(Fuzzy.IRON_INGOT);
         }
-        if (IR_TIE.example() == null) {
+        if (IR_STEEL_BLOCK.example().isEmpty()) {
+            IR_STEEL_BLOCK.addAll(Fuzzy.IRON_BLOCK);
+        }
+        if (IR_TIE.example().isEmpty()) {
             IR_TIE.addAll(Fuzzy.WOOD_PLANK);
         }
-        if (IR_RAIL.example() == null) {
+        if (IR_RAIL.example().isEmpty()) {
             IR_RAIL.add(IRItems.ITEM_RAIL);
         }
-        if (IR_RAIL_BED.example() == null) {
+        if (IR_RAIL_BED.example().isEmpty()) {
             IR_RAIL_BED.addAll(Fuzzy.GRAVEL_BLOCK);
         }
     }
