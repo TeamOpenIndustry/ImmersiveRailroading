0824d856fbb48fae1748ef353bafc788225f6e74
Christian Mesh <christianmesh1@gmail.com>
2019-10-13 08:55:56 -0400

    More setup tweaks

--- setup.ps1
+++ setup.ps1
@@ -13,4 +13,5 @@ git clone --branch $branch git@github.com:TeamOpenIndustry/UniversalModCore.git
 git clone --branch $branch git@github.com:TeamOpenIndustry/ImmersiveRailroadingIntegration.git
 
 ./UniversalModCore/template/setup.ps1 $branch immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
-(Get-Content build.gradle) -replace '^dependencies {', "apply from: 'ImmersiveRailroadingIntegration/dependencies.gradle'`r`ndependencies {" | Set-Content build.gradle 
+
+./ImmersiveRailroadingIntegration/setu.ps1 $branch
--- setup.sh
+++ setup.sh
@@ -11,4 +11,5 @@ git clone --branch $branch git@github.com:TeamOpenIndustry/UniversalModCore.git
 git clone --branch $branch git@github.com:TeamOpenIndustry/ImmersiveRailroadingIntegration.git
 
 ./UniversalModCore/template/setup.sh $branch immersiverailroading ImmersiveRailroading 1.6.1 cam72cam.immersiverailroading.ImmersiveRailroading
-sed -i build.gradle -e "s,^dependencies {,apply from: 'ImmersiveRailroadingIntegration/dependencies.gradle'\n\0,"
+
+./ImmersiveRailroadingIntegration/setup.sh $branch
