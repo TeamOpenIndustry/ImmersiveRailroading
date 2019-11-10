0f44fb9296456171c4d65c5a4a57882fc7d795df
Christian Mesh <christianmesh1@gmail.com>
2019-11-10 15:40:35 -0500

    Immersive Railroading 1.7.0

--- src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
+++ src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
@@ -48,7 +48,7 @@ import java.util.function.Function;
 public class ImmersiveRailroading extends ModCore.Mod {
     public static final String MODID = "immersiverailroading";
 	public static final String NAME = "ImmersiveRailroading";
-    public static final String VERSION = "1.6.1";
+    public static final String VERSION = "1.7.0";
 
 	public static final int ENTITY_SYNC_DISTANCE = 512;
 	private static ImmersiveRailroading instance;
