9fe90225b6e95c41fa16d3ba6efde43d67e1dd54
Christian Mesh <christianmesh1@gmail.com>
2019-11-14 07:35:07 -0500

    Add useHttps flag to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 branch=$1
+useHttps=$2
 
 if [ -z "$branch" ]; then
-		branch="forge_1.12.2"
+	branch="forge_1.12.2"
+fi
+
+gitPfx="git@github.com:"
+if [ ! -z "$useHttps" ]; then
+	gitPfx="https://github.com/"
 fi
 
 rm -rf ./UniversalModCore
 rm -rf ./ImmersiveRailroadingIntegration
 
-git clone --branch $branch git@github.com:TeamOpenIndustry/UniversalModCore.git
-git clone --branch $branch git@github.com:TeamOpenIndustry/ImmersiveRailroadingIntegration.git
+git clone --branch $branch ${gitPfx}TeamOpenIndustry/UniversalModCore.git
+git clone --branch $branch ${gitPfx}TeamOpenIndustry/ImmersiveRailroadingIntegration.git
 
 ./UniversalModCore/template/setup.sh $branch immersiverailroading ImmersiveRailroading 1.7.1 cam72cam.immersiverailroading.ImmersiveRailroading
 
-./ImmersiveRailroadingIntegration/setup.sh $branch
+./ImmersiveRailroadingIntegration/setup.sh $branch $useHttps
