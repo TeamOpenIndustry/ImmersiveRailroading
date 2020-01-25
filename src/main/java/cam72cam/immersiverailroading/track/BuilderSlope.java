3ae336e93513ebb34292a8328709d3527e4026ab
Christian Mesh <christianmesh1@gmail.com>
2020-01-25 10:13:46 -0500

    Fix TeamOpenIndustry/ImmersiveRailroading#670

--- src/main/java/cam72cam/immersiverailroading/track/BuilderSlope.java
+++ src/main/java/cam72cam/immersiverailroading/track/BuilderSlope.java
@@ -1,27 +1,21 @@
 package cam72cam.immersiverailroading.track;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import cam72cam.immersiverailroading.util.RailInfo;
-import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.math.Vec3i;
 
 public class BuilderSlope extends BuilderStraight {
 	public BuilderSlope(RailInfo info, Vec3i pos) {
 		super(info, pos);
 	}
-	
-	public List<PosStep> getPath(double stepSize) {
-		List<PosStep> res = new ArrayList<PosStep>();
-		
-		float slope = (1.0F/(info.settings.length));
-		float startHeight = slope/2;
 
-		for (PosStep cur : super.getPath(stepSize)) {
-			res.add(new PosStep(cur.x, cur.y + startHeight + slope * cur.distanceTo(Vec3d.ZERO), cur.z, cur.yaw, (float)-Math.toDegrees(Math.atan(slope))));
-		}
-		
-		return res;
+	@Override
+	public CubicCurve getCurve() {
+		CubicCurve curve = super.getCurve();
+		return new CubicCurve(
+				curve.p1,
+				curve.ctrl1.add(0, 0.25, 0),
+				curve.ctrl2.add(0, 0.75, 0),
+				curve.p2.add(0, 1, 0)
+		);
 	}
 }
