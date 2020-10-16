84b6c9618d0c7c5b0063a873a8612ee511690319
Christian Mesh <christianmesh1@gmail.com>
2020-10-16 08:50:32 -0400

    Add Tag annotations to SoundPacket

--- src/main/java/cam72cam/immersiverailroading/net/SoundPacket.java
+++ src/main/java/cam72cam/immersiverailroading/net/SoundPacket.java
@@ -2,18 +2,26 @@ package cam72cam.immersiverailroading.net;
 
 import cam72cam.immersiverailroading.ImmersiveRailroading;
 import cam72cam.immersiverailroading.library.Gauge;
+import cam72cam.mod.serialization.TagField;
 import cam72cam.mod.sound.ISound;
 import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.net.Packet;
 import cam72cam.mod.resource.Identifier;
 
 public class SoundPacket extends Packet {
+	@TagField
 	private String file;
+	@TagField
 	private Vec3d pos;
+	@TagField
 	private Vec3d motion;
+	@TagField
 	private float volume;
+	@TagField
 	private float pitch;
+	@TagField
 	private int distance;
+	@TagField
 	private double gauge;
 
 	public SoundPacket() { }
