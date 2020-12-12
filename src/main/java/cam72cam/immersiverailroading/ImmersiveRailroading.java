136ed9b39f5fd7f4e2561f947baa7a23c440f365
Christian Mesh <christianmesh1@gmail.com>
2020-12-12 14:44:31 -0500

    Remove unused props

--- src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
+++ src/main/java/cam72cam/immersiverailroading/ImmersiveRailroading.java
@@ -48,8 +48,6 @@ import java.util.function.Function;
 
 public class ImmersiveRailroading extends ModCore.Mod {
     public static final String MODID = "immersiverailroading";
-	public static final String NAME = "ImmersiveRailroading";
-    public static final String VERSION = "1.7.3";
 
 	public static final int ENTITY_SYNC_DISTANCE = 512;
 	private static ImmersiveRailroading instance;
