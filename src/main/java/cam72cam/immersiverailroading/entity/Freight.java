d1fff34baf85415b227935f2406a8e0495ce0169
Appw <austinpaulpal@gmail.com>
2021-05-12 23:03:55 -0700

    Use Item Hacky Fix
    A hacky fix to allow some items to be used while riding a train

--- src/main/java/cam72cam/immersiverailroading/entity/Freight.java
+++ src/main/java/cam72cam/immersiverailroading/entity/Freight.java
@@ -106,9 +106,11 @@ public abstract class Freight extends EntityCoupleableRollingStock {
 			}
 		}
 		
-		if (guiType() != null) {
+		if (guiType() != null && player.getHeldItem(Player.Hand.PRIMARY).isEmpty() && player.getHeldItem(Player.Hand.SECONDARY).isEmpty()) {
 			guiType().open(player, this);
 			return ClickResult.ACCEPTED;
+		} else if (!player.getHeldItem(Player.Hand.PRIMARY).isEmpty()) {
+			player.getHeldItem(Player.Hand.PRIMARY).use(player.getWorld(), player, Player.Hand.PRIMARY);
 		}
 		return ClickResult.PASS;
 	}
