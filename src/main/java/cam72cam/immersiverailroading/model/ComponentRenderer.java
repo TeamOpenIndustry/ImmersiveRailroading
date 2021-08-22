10fa3d8963100351c18c0dfc88f8d8ed94947185
Christian Mesh <christianmesh1@gmail.com>
2021-08-22 08:43:57 -0400

    Improve how interior lighting groups are calculated

--- src/main/java/cam72cam/immersiverailroading/model/ComponentRenderer.java
+++ src/main/java/cam72cam/immersiverailroading/model/ComponentRenderer.java
@@ -10,6 +10,7 @@ import org.lwjgl.opengl.GL11;
 import java.io.Closeable;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,17 +22,17 @@ public class ComponentRenderer implements Closeable {
     private final Float interiorLight;
     private final Float skyLight;
     private final boolean fullbright;
-    private final boolean useInteriorFallback;
+    private final boolean hasInterior;
 
-    public ComponentRenderer(BoundOBJVBO vbo, List<ModelComponentType> available, boolean useInteriorFallback) {
-        this(vbo, available, false, useInteriorFallback, false, null, null);
+    public ComponentRenderer(BoundOBJVBO vbo, List<ModelComponentType> available, boolean hasInterior) {
+        this(vbo, available, false, hasInterior, false, null, null);
     }
 
-    public ComponentRenderer(BoundOBJVBO vbo, List<ModelComponentType> available, boolean newMatrix, boolean useInteriorFallback, boolean fullbright, Float interiorLight, Float skyLight) {
+    public ComponentRenderer(BoundOBJVBO vbo, List<ModelComponentType> available, boolean newMatrix, boolean hasInterior, boolean fullbright, Float interiorLight, Float skyLight) {
         this.vbo = vbo;
         this.available = available;
         this.fullbright = fullbright;
-        this.useInteriorFallback = useInteriorFallback;
+        this.hasInterior = hasInterior;
         this.interiorLight = interiorLight;
         this.skyLight = skyLight;
         matrix = newMatrix ? OpenGL.matrix() : () -> {};
@@ -56,63 +57,72 @@ public class ComponentRenderer implements Closeable {
     }
 
     public ComponentRenderer withBrightGroups(boolean fullbright) {
-        return new ComponentRenderer(vbo, available, false, useInteriorFallback, fullbright, interiorLight, skyLight);
+        return new ComponentRenderer(vbo, available, false, hasInterior, fullbright, interiorLight, skyLight);
     }
 
     public ComponentRenderer withInteriorLight(EntityRollingStock stock) {
         float interiorLight = 6 / 15f;
         float blockLight = stock.getWorld().getBlockLightLevel(stock.getBlockPosition());
         float skyLight = stock.getWorld().getSkyLightLevel(stock.getBlockPosition());
-        return blockLight < interiorLight ? new ComponentRenderer(vbo, available, false, useInteriorFallback, fullbright, interiorLight, skyLight) : this;
+        return blockLight < interiorLight ? new ComponentRenderer(vbo, available, false, hasInterior, fullbright, interiorLight, skyLight) : this;
     }
 
     public ComponentRenderer push() {
-        return new ComponentRenderer(vbo, available, true, useInteriorFallback, fullbright, interiorLight, skyLight);
+        return new ComponentRenderer(vbo, available, true, hasInterior, fullbright, interiorLight, skyLight);
     }
 
-    private void draw(Collection<String> groups, boolean fullbright) {
-        if (fullbright) {
-            // Is partitioning faster?
-            List<String> std = groups.stream().filter(x -> !x.contains("FULLBRIGHT")).collect(Collectors.toList());
-            List<String> bright = groups.stream().filter(x -> x.contains("FULLBRIGHT")).collect(Collectors.toList());
-            if (!std.isEmpty()) {
-                if (interiorLight != null) {
-                    List<String> exterior = groups.stream().filter(x -> !x.contains("INTERIOR")).collect(Collectors.toList());
-                    List<String> interior = groups.stream().filter(x -> x.contains("INTERIOR")).collect(Collectors.toList());
-                    if (!interior.isEmpty()) {
-                        try (OpenGL.With lm = OpenGL.lightmap(interiorLight, skyLight)) {
-                            vbo.draw(interior);
-                        }
-                    }
-                    if (!exterior.isEmpty()) {
-                        try (OpenGL.With lm = interior.isEmpty() && useInteriorFallback ? OpenGL.lightmap(interiorLight, skyLight) : () -> {}) {
-                            vbo.draw(exterior);
-                        }
-                    }
-                } else {
-                    vbo.draw(std);
-                }
-            }
-            if (!bright.isEmpty()) {
-                try (
-                        OpenGL.With light = OpenGL.shaderActive() ?
-                                OpenGL.lightmap(1, 1) :
-                                OpenGL.bool(GL11.GL_LIGHTING, false).and(OpenGL.lightmap(false));
-                ) {
-                    vbo.draw(bright);
-                }
-            }
-        } else {
+    private void draw(Collection<String> groups) {
+        if (interiorLight == null && !fullbright) {
+            // Skip any sort of lighting logic
             vbo.draw(groups);
+            return;
+        }
+
+
+        List<String> noop = Collections.EMPTY_LIST;
+        List<String> interiorNormal = hasInterior ? new ArrayList<>() : noop;
+        List<String> interiorFullbright = hasInterior && interiorLight != null && fullbright ? new ArrayList<>() : noop;
+        List<String> exteriorNormal = new ArrayList<>();
+        List<String> exteriorFullbright = fullbright ? new ArrayList<>() : noop;
+
+        for (String group : groups) {
+            if (hasInterior && group.contains("INTERIOR")) {
+                (fullbright && interiorLight != null && group.contains("FULLBRIGHT") ? interiorFullbright : interiorNormal).add(group);
+            } else {
+                (fullbright && group.contains("FULLBRIGHT") ? exteriorFullbright : exteriorNormal).add(group);
+            }
+        }
+
+        if (!interiorFullbright.isEmpty() || !exteriorFullbright.isEmpty()) {
+            try (
+                    OpenGL.With light = OpenGL.shaderActive() ?
+                            OpenGL.lightmap(1, 1) :
+                            OpenGL.bool(GL11.GL_LIGHTING, false).and(OpenGL.lightmap(false));
+            ) {
+                vbo.draw(exteriorFullbright);
+                vbo.draw(interiorFullbright);
+            }
+        }
+
+        if (!interiorNormal.isEmpty()) {
+            try (OpenGL.With lm = interiorLight != null ? OpenGL.lightmap(interiorLight, skyLight) : () -> {}) {
+                vbo.draw(interiorNormal);
+            }
+        }
+
+        if (!exteriorNormal.isEmpty()) {
+            try (OpenGL.With lm = interiorLight != null && interiorNormal.isEmpty() ? OpenGL.lightmap(interiorLight, skyLight) : () -> {}) {
+                vbo.draw(exteriorNormal);
+            }
         }
     }
 
     @Override
     public void close() {
         if (buffer.size() == 1) {
-            draw(buffer.get(0).modelIDs, fullbright);
+            draw(buffer.get(0).modelIDs);
         } else if (buffer.size() > 1) {
-            draw(buffer.stream().flatMap(x -> x.modelIDs.stream()).collect(Collectors.toList()), fullbright);
+            draw(buffer.stream().flatMap(x -> x.modelIDs.stream()).collect(Collectors.toList()));
         }
         matrix.restore();
     }
