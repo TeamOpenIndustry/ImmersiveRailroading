f8acec06663ba89de11a56209f8e21c8bdc70d00
Christian Mesh <christianmesh1@gmail.com>
2021-08-29 19:36:54 -0400

    Add support for disabling clacking in track definitions
    "clack": false

--- src/main/java/cam72cam/immersiverailroading/tile/TileRailBase.java
+++ src/main/java/cam72cam/immersiverailroading/tile/TileRailBase.java
@@ -959,4 +959,8 @@ public class TileRailBase extends BlockEntityTrackTickable implements IRedstoneP
 		}
 		return true;
 	}
+
+    public boolean clacks() {
+		return getParent() != null && getParentTile().clacks();
+    }
 }
