12e34331cee982a10ba51ae064724c43d720902c
Christian Mesh <christianmesh1@gmail.com>
2021-09-03 11:36:46 -0400

    Fix engine turned on with multiple controls

--- src/main/java/cam72cam/immersiverailroading/entity/LocomotiveDiesel.java
+++ src/main/java/cam72cam/immersiverailroading/entity/LocomotiveDiesel.java
@@ -273,10 +273,9 @@ public class LocomotiveDiesel extends Locomotive {
 	public void onDragRelease(Control component) {
 		super.onDragRelease(component);
 		if (component.part.type == ModelComponentType.ENGINE_START_X) {
-			setTurnedOn(getDefinition().getModel().getDraggableComponents().stream()
+			turnedOn = getDefinition().getModel().getDraggableComponents().stream()
 					.filter(c -> c.part.type == ModelComponentType.ENGINE_START_X)
-					.allMatch(c -> getControlPosition(c) == 1)
-			);
+					.allMatch(c -> getControlPosition(c) == 1);
 		}
 		if (component.part.type == ModelComponentType.REVERSER_X) {
 			// Make sure reverser is sync'd
