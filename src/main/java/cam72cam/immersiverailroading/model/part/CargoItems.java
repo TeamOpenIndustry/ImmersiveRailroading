7012c6a4a8aaf755f002e7988a998b119553c1a6
Christian Mesh <christianmesh1@gmail.com>
2022-12-15 10:20:09 -0500

    Support for rendering multiple internal cargo item areas

--- src/main/java/cam72cam/immersiverailroading/model/part/CargoItems.java
+++ src/main/java/cam72cam/immersiverailroading/model/part/CargoItems.java
@@ -4,6 +4,7 @@ import cam72cam.immersiverailroading.entity.Freight;
 import cam72cam.immersiverailroading.library.ModelComponentType;
 import cam72cam.immersiverailroading.model.components.ComponentProvider;
 import cam72cam.immersiverailroading.model.components.ModelComponent;
+import cam72cam.mod.item.ItemStack;
 import cam72cam.mod.render.StandardModel;
 import cam72cam.mod.render.opengl.RenderState;
 import util.Matrix4;
@@ -38,39 +39,48 @@ public class CargoItems {
         if (model == null) {
             model = new StandardModel();
 
-            ModelComponent area = areas.get(0);
+            double totalVolume = areas.stream().mapToDouble(a -> a.length() * a.width() * a.height()).sum();
+            int slotOffset = 0;
+            for (ModelComponent area : areas) {
+                double itemsX = area.length();
+                double itemsY = area.height();
+                double itemsZ = area.width();
 
-            double cargoVolume = stock.cargoItems.getSlotCount();
+                double modelVolume = itemsX * itemsY * itemsZ;
+                int cargoVolume = (int) Math.ceil(stock.cargoItems.getSlotCount() * modelVolume/totalVolume);
 
-            double itemsX = area.length();
-            double itemsY = area.height();
-            double itemsZ = area.width();
+                double volumeRatio = Math.pow(cargoVolume / modelVolume, 0.3333);
+                itemsY = Math.ceil(itemsY * volumeRatio);
 
-            double modelVolume = itemsX * itemsY * itemsZ;
-            double volumeRatio = Math.pow(cargoVolume / modelVolume, 0.3333);
-            itemsY = Math.ceil(itemsY * volumeRatio);
+                modelVolume = itemsX * itemsY * itemsZ;
+                volumeRatio = Math.pow(cargoVolume / modelVolume, 0.3333);
+                itemsZ = Math.ceil(itemsZ * volumeRatio);
 
-            modelVolume = itemsX * itemsY * itemsZ;
-            volumeRatio = Math.pow(cargoVolume / modelVolume, 0.3333);
-            itemsZ = Math.ceil(itemsZ * volumeRatio);
+                modelVolume = itemsX * itemsY * itemsZ;
+                volumeRatio = cargoVolume / modelVolume;
+                itemsX = Math.ceil(itemsX * volumeRatio);
 
-            modelVolume = itemsX * itemsY * itemsZ;
-            volumeRatio = Math.pow(cargoVolume / modelVolume, 0.3333);
-            itemsX = Math.ceil(itemsX * volumeRatio);
+                int renderSlot = 0;
+                for (int i = slotOffset; i < Math.min(slotOffset+ cargoVolume, stock.cargoItems.getSlotCount()); i++) {
+                    ItemStack stack = stock.cargoItems.get(i);
+                    if (stack.isEmpty()) {
+                        continue;
+                    }
+                    int x = renderSlot % (int) itemsX;
+                    int z = (renderSlot / (int) itemsX) % (int) itemsZ;
+                    int y = (renderSlot / (int) itemsX / (int) itemsZ) % (int) itemsY;
 
-            for (int i = 0; i < stock.cargoItems.getSlotCount(); i++) {
-                int x = i % (int) itemsX;
-                int z = (i / (int) itemsX) % (int) itemsZ;
-                int y = (i / (int) itemsX / (int) itemsZ) % (int) itemsY;
-
-                model.addItem(
-                        stock.cargoItems.get(i),
-                        new Matrix4().
-                                translate(area.min.x, area.min.y, area.min.z)
-                                .scale(area.length() / itemsX, area.height() / itemsY, area.width() / itemsZ)
-                                .translate(0.5, 0.5, 0.5)
-                                .translate(x, y, z)
-                );
+                    model.addItem(
+                            stack,
+                            new Matrix4().
+                                    translate(area.min.x, area.min.y, area.min.z)
+                                    .scale(area.length() / itemsX, area.height() / itemsY, area.width() / itemsZ)
+                                    .translate(0.5, 0.5, 0.5)
+                                    .translate(x, y, z)
+                    );
+                    renderSlot++;
+                }
+                slotOffset += cargoVolume;
             }
             cache.put(stock.getUUID(), model);
         }
