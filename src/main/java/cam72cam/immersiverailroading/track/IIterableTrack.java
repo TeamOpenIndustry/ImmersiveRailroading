eee05f2b03b1ad6c2be165a683903a2fbb83655a
Christian Mesh <christianmesh1@gmail.com>
2023-10-20 08:05:29 -0400

    Revert "Switch to binary tree pathing in offsetFromTrack"
    This reverts commit 2957c0588d1b70751ced3c016a7c5d59f5f6c134.

--- src/main/java/cam72cam/immersiverailroading/track/IIterableTrack.java
+++ src/main/java/cam72cam/immersiverailroading/track/IIterableTrack.java
@@ -12,39 +12,17 @@ public interface IIterableTrack {
     List<BuilderBase> getSubBuilders();
 
     default double offsetFromTrack(RailInfo info, Vec3i pos, Vec3d position) {
+        double distSquared = 100 * 100;
 
         // Convert to relative
-        Vec3d relative = position.subtract(info.placementInfo.placementPosition).subtract(pos);
-        relative = relative.add(0, -(relative.y % 1), 0);
+        position = position.subtract(info.placementInfo.placementPosition).subtract(pos);
 
-        List<PosStep> positions = getPath(info.settings.gauge.scale() / 8);
-
-        /*
-        double distSquared = 100 * 100;
-        for (Vec3d gagPos : positions) {
-            double offSquared = gagPos.distanceToSquared(relative);
+        for (Vec3d gagPos : getPath(info.settings.gauge.scale()/8)) {
+            double offSquared = gagPos.distanceToSquared(position.add(0, -(position.y % 1), 0));
             if (offSquared < distSquared) {
                 distSquared = offSquared;
             }
         }
         return Math.sqrt(distSquared);
-        */
-
-        int left = 0;
-        double leftDistance = positions.get(left).distanceToSquared(relative);
-        int right = positions.size() - 1;
-        double rightDistance = positions.get(right).distanceToSquared(relative);
-        while (right - left > 1) {
-            double midIdx = left + (right - left) / 2f;
-
-            if (leftDistance > rightDistance) {
-                left = (int) Math.floor(midIdx);
-                leftDistance = positions.get(left).distanceToSquared(relative);
-            } else {
-                right = (int) Math.ceil(midIdx);
-                rightDistance = positions.get(right).distanceToSquared(relative);
-            }
-        }
-        return Math.sqrt(Math.min(rightDistance, leftDistance));
     }
 }
