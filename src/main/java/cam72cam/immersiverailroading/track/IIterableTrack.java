3a663a1941d3557570e8925cc6f477b3ddce0125
Christian Mesh <christianmesh1@gmail.com>
2023-10-20 10:19:36 -0400

    Switch closest track pathing search to step by 1/3rds
    Fixes some non-linear + rounding issues

--- src/main/java/cam72cam/immersiverailroading/track/IIterableTrack.java
+++ src/main/java/cam72cam/immersiverailroading/track/IIterableTrack.java
@@ -1,5 +1,6 @@
 package cam72cam.immersiverailroading.track;
 
+import cam72cam.immersiverailroading.ImmersiveRailroading;
 import cam72cam.immersiverailroading.util.RailInfo;
 import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.math.Vec3i;
@@ -19,32 +20,35 @@ public interface IIterableTrack {
 
         List<PosStep> positions = getPath(info.settings.gauge.scale() / 8);
 
-        /*
-        double distSquared = 100 * 100;
+        /*double distSquared = 100 * 100;
         for (Vec3d gagPos : positions) {
             double offSquared = gagPos.distanceToSquared(relative);
             if (offSquared < distSquared) {
                 distSquared = offSquared;
             }
-        }
-        return Math.sqrt(distSquared);
-        */
+        }*/
 
         int left = 0;
         double leftDistance = positions.get(left).distanceToSquared(relative);
         int right = positions.size() - 1;
         double rightDistance = positions.get(right).distanceToSquared(relative);
         while (right - left > 1) {
-            double midIdx = left + (right - left) / 2f;
-
             if (leftDistance > rightDistance) {
-                left = (int) Math.floor(midIdx);
+                left = (int) Math.ceil(left + (right - left) / 3f);
                 leftDistance = positions.get(left).distanceToSquared(relative);
             } else {
-                right = (int) Math.ceil(midIdx);
+                right = (int) Math.floor(right + (left - right) / 3f);
                 rightDistance = positions.get(right).distanceToSquared(relative);
             }
         }
-        return Math.sqrt(Math.min(rightDistance, leftDistance));
+
+
+        double resultSquared = Math.min(rightDistance, leftDistance);
+
+        /*if (resultSquared != distSquared) {
+            ImmersiveRailroading.info("orig: %s, new %s", Math.sqrt(distSquared), Math.sqrt(resultSquared));
+        }*/
+
+        return Math.sqrt(resultSquared);
     }
 }
