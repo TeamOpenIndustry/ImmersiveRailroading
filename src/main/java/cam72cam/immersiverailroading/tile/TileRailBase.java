d1d1db587c9a42a2a1041c55cb039f2c039daf3b
Christian Mesh <christianmesh1@gmail.com>
2023-12-14 17:07:32 -0500

    Fix showAccumulation = 0 causing infinite snow

--- src/main/java/cam72cam/immersiverailroading/tile/TileRailBase.java
+++ src/main/java/cam72cam/immersiverailroading/tile/TileRailBase.java
@@ -513,7 +513,7 @@ public class TileRailBase extends BlockEntityTrackTickable implements IRedstoneP
 		
 		ticksExisted += 1;
 
-		if (((int) (Math.random() * ConfigDebug.snowAccumulateRate * 10) == 0)) {
+		if (ConfigDebug.snowAccumulateRate > 0 && ((int) (Math.random() * ConfigDebug.snowAccumulateRate * 10) == 0)) {
 			if (getWorld().isSnowing(getPos()) && getWorld().canSeeSky(getPos().up())) {
 				this.handleSnowTick();
 			}
