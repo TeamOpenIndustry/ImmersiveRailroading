a19bd58b4a8ca731016bb032ec16c91214881190
Deepseasaltyfish <h20040215@163.com>
2026-02-10 16:15:12 +0800

    change height offset range

--- src/main/java/cam72cam/immersiverailroading/gui/TrackGui.java
+++ src/main/java/cam72cam/immersiverailroading/gui/TrackGui.java
@@ -465,26 +465,18 @@ public class TrackGui implements IScreen {
 		};
 		ytop += height;
 
-		nearHeightOffsetSlider = new Slider(screen, GUIHelpers.getScreenWidth() / 2 - width + 50, ytop + height, "", 0.0, 1.0, settings.posOffsetTag.getFloat("placementOffset"), true) {
+		nearHeightOffsetSlider = new Slider(screen, GUIHelpers.getScreenWidth() / 2 - width + 50, ytop + height, "", -0.5, 0.5, settings.posOffsetTag.getFloat("placementOffset"), true) {
 			@Override
 			public void onSlider() {
-				if(Math.abs(1-nearHeightOffsetSlider.getValue())<1e-6) {
-					nearHeightOffsetSlider.setValue(0.99);
-				}
-
 				settings.posOffsetTag.setFloat("placementOffset",(float) this.getValue());
 				nearHeightOffsetSlider.setText(GuiText.TRACK_NEAR_HEIGHT_OFFSET.toString(String.format("%.2f", settings.posOffsetTag.getFloat("placementOffset"))));
 				updateListSetting(mutable -> mutable.posOffsetTag.setFloat("placementOffset",(float) this.getValue()));
 			}
 		};
 		nearHeightOffsetSlider.onSlider();
-		farHeightOffsetSlider = new Slider(screen, GUIHelpers.getScreenWidth() / 2 - width + 50, ytop + height * 2, "", 0.0, 1.0, selectedWay==0 ? settings.posOffsetTag.getFloat("customOffset") : selectedWaySettings.posOffsetTag.getFloat("customOffset"), true) {
+		farHeightOffsetSlider = new Slider(screen, GUIHelpers.getScreenWidth() / 2 - width + 50, ytop + height * 2, "", -0.5, 0.5, selectedWay==0 ? settings.posOffsetTag.getFloat("customOffset") : selectedWaySettings.posOffsetTag.getFloat("customOffset"), true) {
 			@Override
 			public void onSlider() {
-				if(Math.abs(farHeightOffsetSlider.getValue() - 1) < 1e-6) {
-					farHeightOffsetSlider.setValue(0.99);
-				}
-
 				if(selectedWay == 0) {
 					settings.posOffsetTag.setFloat("customOffset",(float) this.getValue());
 					farHeightOffsetSlider.setText(GuiText.TRACK_FAR_HEIGHT_OFFSET.toString(String.format("%.2f", settings.posOffsetTag.getFloat("customOffset"))));
