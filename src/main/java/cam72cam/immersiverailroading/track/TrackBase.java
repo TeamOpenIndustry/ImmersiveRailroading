842148036aa664e21be3380caf714db4f7ca8cf3
Deepseasaltyfish <h20040215@163.com>
2026-07-05 00:33:58 +0800

    clean

--- src/main/java/cam72cam/immersiverailroading/track/TrackBase.java
+++ src/main/java/cam72cam/immersiverailroading/track/TrackBase.java
@@ -7,7 +7,6 @@ import cam72cam.immersiverailroading.tile.TileRail;
 import cam72cam.immersiverailroading.tile.TileRailBase;
 import cam72cam.immersiverailroading.tile.TileRailGag;
 import cam72cam.immersiverailroading.util.BlockUtil;
-import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.math.Vec3i;
 import cam72cam.mod.serialization.TagCompound;
 import cam72cam.mod.util.SingleCache;
@@ -20,7 +19,6 @@ public abstract class TrackBase {
 
 	protected Vec3i rel;
 	private float bedHeight;
-//	private Vec3d topFacing;
 	private float railHeight;
 	//Override default value
 	private boolean scaleModel = true;
@@ -78,7 +76,6 @@ public abstract class TrackBase {
 			}
 			tr.setRailHeight(getRailHeight());
 			tr.setBedHeight(getBedHeight());
-//			tr.setTopFacing(getTopFacing());
 			tr.setScaleModel(isScaleModel());
 			return tr;
 		}
@@ -119,7 +116,6 @@ public abstract class TrackBase {
 			tr.setParent(builder.getParentPos());
 		}
 		tr.setRailHeight(getRailHeight());
-//		tr.setTopFacing(getTopFacing());
 		tr.setBedHeight(getBedHeight());
 		tr.setScaleModel(isScaleModel());
 		for (int i = 0; i < hasSnow; i++) {
@@ -140,11 +136,9 @@ public abstract class TrackBase {
 	public void setBedHeight(float height) {
 		this.bedHeight = height;
 	}
-//	public void setTopFacing(Vec3d topFacing) { this.topFacing = topFacing; }
 	public float getBedHeight() {
 		return bedHeight;
 	}
-//	public Vec3d getTopFacing() { return topFacing; }
 	public void setRailHeight(float height) {
 		this.railHeight = height;
 	}
