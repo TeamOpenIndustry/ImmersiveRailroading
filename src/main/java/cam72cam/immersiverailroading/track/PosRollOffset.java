0f7b44fcef081eb53ddfdc024587a430df93ae21
Deepseasaltyfish <h20040215@163.com>
2026-07-06 20:37:30 +0800

    refactor (PosRollOffset.java)

--- src/main/java/cam72cam/immersiverailroading/track/PosRollOffset.java
+++ src/main/java/cam72cam/immersiverailroading/track/PosRollOffset.java
@@ -5,39 +5,30 @@ import cam72cam.mod.math.Vec3d;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PosRollOffset {
-    public final Vec3d point;
-    public final double roll;
-    public final double yOffset;
-    public final double zOffset;
-    public PosRollOffset(Vec3d point, double roll, double yOffset, double zOffset) {
-        this.point = point;
-        this.roll = roll;
-        this.yOffset = yOffset;
-        this.zOffset = zOffset;
-    }
+public record PosRollOffset(Vec3d point, double roll, double yOffset, double zOffset) {
 
     public static List<Vec3d> getPoints(List<PosRollOffset> posRollOffset) {
         List<Vec3d> points = new ArrayList<>();
-        for(int i = 0; i < posRollOffset.size(); i++)points.add(posRollOffset.get(i).point);
+        for (PosRollOffset rollOffset : posRollOffset) points.add(rollOffset.point);
         return points;
     }
 
     public static List<Double> getRolls(List<PosRollOffset> posRollOffset) {
         List<Double> points = new ArrayList<>();
-        for(int i = 0; i < posRollOffset.size(); i++)points.add(posRollOffset.get(i).roll);
+        for (PosRollOffset rollOffset : posRollOffset) points.add(rollOffset.roll);
         return points;
     }
 
     public static List<Double> getYOffsets(List<PosRollOffset> posRollOffset) {
         List<Double> points = new ArrayList<>();
-        for(int i = 0; i < posRollOffset.size(); i++)points.add(posRollOffset.get(i).yOffset);
+        for (PosRollOffset rollOffset : posRollOffset) points.add(rollOffset.yOffset);
         return points;
     }
 
     public static List<Double> getZOffsets(List<PosRollOffset> posRollOffset) {
         List<Double> points = new ArrayList<>();
-        for(int i = 0; i < posRollOffset.size(); i++)points.add(posRollOffset.get(i).zOffset);
+        for (PosRollOffset rollOffset : posRollOffset) points.add(rollOffset.zOffset);
         return points;
     }
+
 }
