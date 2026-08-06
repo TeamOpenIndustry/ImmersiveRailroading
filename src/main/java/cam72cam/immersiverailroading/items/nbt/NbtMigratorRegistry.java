199905a4d30a6131e0392f82d655f9171ed66592
Deepseasaltyfish <h20040215@163.com>
2026-08-06 22:51:00 +0800

    simplify NbtMigratorRegistry

--- src/main/java/cam72cam/immersiverailroading/items/nbt/NbtMigratorRegistry.java
+++ src/main/java/cam72cam/immersiverailroading/items/nbt/NbtMigratorRegistry.java
@@ -1,5 +1,6 @@
 package cam72cam.immersiverailroading.items.nbt;
 
+import cam72cam.immersiverailroading.library.TrackPositionType;
 import cam72cam.mod.serialization.TagCompound;
 
 import java.util.ArrayList;
@@ -9,7 +10,20 @@ public class NbtMigratorRegistry {
     private static final List<NbtMigrator> migrations = new ArrayList<>();
 
     static {
-        register(new PosTypeMigrator());
+        register(data -> {
+            if (data.hasKey("pos_type")) {
+                TrackPositionType oldPosType = data.getEnum("pos_type", TrackPositionType.class);
+                TagCompound nearData = data.get("nearPointData");
+                if (nearData == null) {
+                    nearData = new TagCompound();
+                    data.set("nearPointData", nearData);
+                }
+                if (!nearData.hasKey("pos_type")) {
+                    nearData.setEnum("pos_type", oldPosType);
+                }
+                data.remove("pos_type");
+            }
+        });
     }
 
     public static void register(NbtMigrator migration) {
@@ -20,5 +34,10 @@ public class NbtMigratorRegistry {
         for (NbtMigrator migration : migrations) {
             migration.migrate(data);
         }
+    }
+
+    @FunctionalInterface
+    public static interface NbtMigrator {
+        void migrate(TagCompound data);
     }
 }
