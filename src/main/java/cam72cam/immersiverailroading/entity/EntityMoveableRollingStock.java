261792a9bdb10b6bd87f473a72746a07288c274b
Deepseasaltyfish <h20040215@163.com>
2026-08-22 16:23:34 +0800

    fix stock snow clearing

--- src/main/java/cam72cam/immersiverailroading/entity/EntityMoveableRollingStock.java
+++ src/main/java/cam72cam/immersiverailroading/entity/EntityMoveableRollingStock.java
@@ -257,7 +257,7 @@ public abstract class EntityMoveableRollingStock extends EntityRidableRollingSto
                 for (Vec3i bp : state.trackToUpdate) {
                     TileRailBase te = getWorld().getBlockEntity(bp, TileRailBase.class);
                     if (te != null) {
-                        te.cleanSnow(this.getDefinition().getSnowLayers());
+                        te.cleanSnow(te.getMinSnowLayers() + this.getDefinition().getSnowLayers());
                         te.stockOverhead(this);
                     }
                 }
